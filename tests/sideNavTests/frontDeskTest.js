863fba7370094f7f684b9bbcca270e0ead672063
Ray Wagner <raywagner88@me.com>
2018-05-03 15:18:34 -0500

    Add front desk test case

---
+++ tests/sideNavTests/frontDeskTest.js
@@ -0,0 +1,57 @@
+module.exports = {
+    tags: ['frontDeskTest', 'menuTests'],
+
+    'Front Desk Test': (client) => {
+
+        var page = client.page.login();
+
+        var schoolID = '1';
+        var username = 'superuser';
+        var password = 'guest';
+
+        page.navigate()
+            .schoolLogin(username, password, schoolID);
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@frontDesk')
+            .assert.containsText('@frontDesk', '» Front Desk')
+
+            .assert.hidden('@frontDeskFamilyDirectory')
+            .assert.hidden('@frontDeskEmployeeDirectory')
+            .assert.hidden('@frontDeskClassroomDirectory')
+            .assert.hidden('@frontDeskDepartments')
+
+            .click('@frontDesk')
+
+            .assert.visible('@frontDesk')
+            .assert.visible('@frontDeskFamilyDirectory')
+            .assert.visible('@frontDeskEmployeeDirectory')
+            .assert.visible('@frontDeskClassroomDirectory')
+            .assert.visible('@frontDeskDepartments')
+
+            .assert.containsText('@frontDeskFamilyDirectory', 'Family Directory')
+            .assert.containsText('@frontDeskEmployeeDirectory', 'Employee Directory')
+            .assert.containsText('@frontDeskClassroomDirectory', 'Classroom Directory')
+            .assert.containsText('@frontDeskDepartments', 'Departments')
+
+            .assert.attributeEquals('@frontDeskFamilyDirectory', 'href', 'http://web/directory.php?task=view&grade=-1')
+            .assert.attributeEquals('@frontDeskEmployeeDirectory', 'href', 'http://web/directory.php?task=staffphoto')
+            .assert.attributeEquals('@frontDeskClassroomDirectory', 'href', 'http://web/directory.php?task=classes')
+            .assert.attributeEquals('@frontDeskDepartments', 'href', 'http://web/departments.php?task=browse')
+
+
+            .click('@frontDesk')
+
+            .assert.visible('@frontDesk')
+
+            .assert.hidden('@frontDeskFamilyDirectory')
+            .assert.hidden('@frontDeskEmployeeDirectory')
+            .assert.hidden('@frontDeskClassroomDirectory')
+            .assert.hidden('@frontDeskDepartments')
+
+        client.frame(null);
+
+        client.end();
+    }
+};
