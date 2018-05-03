6e23c6d3c66a5b8d199dd69354a9c4ad3ccd07cd
Ray Wagner <raywagner88@me.com>
2018-05-03 16:01:48 -0500

    Add comm center test case

---
+++ tests/sideNavTests/commCenterTest.js
@@ -0,0 +1,47 @@
+module.exports = {
+    tags: ['commCenterTest', 'menuTests'],
+
+    'Comm Center Test': (client) => {
+
+        var page = client.page.login();
+
+        var schoolID = '1';
+        var username = 'superuser';
+        var password = 'guest';
+
+        page.navigate()
+            .schoolLogin(username, password, schoolID);
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@commCenter')
+            .assert.containsText('@commCenter', '» Comm Center')
+
+            .assert.hidden('@commCenterBlogs')
+            .assert.hidden('@commCenterBlogs')
+
+            .click('@commCenter')
+
+            .assert.visible('@commCenter')
+            .assert.visible('@commCenterBlogs')
+            .assert.visible('@commCenterDiscussions')
+
+            .assert.containsText('@commCenterBlogs', 'Blogs')
+            .assert.containsText('@commCenterDiscussions', 'Discussions')
+
+            .assert.attributeEquals('@commCenterBlogs', 'href', 'http://web/blogs.php')
+            .assert.attributeEquals('@commCenterDiscussions', 'href', 'http://web/forumboard.php?pid=0')
+
+
+            .click('@commCenter')
+
+            .assert.visible('@commCenter')
+
+            .assert.hidden('@commCenterBlogs')
+            .assert.hidden('@commCenterDiscussions')
+
+        client.frame(null);
+
+        client.end();
+    }
+};
