da2d2169fc931d2097c3604f1353eff7cb5c62d3
Ray Wagner <raywagner88@me.com>
2018-05-15 16:57:27 -0500

    Add email menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -563,6 +563,13 @@ module.exports = {
         passANoteFamilies: '#menu > li:nth-child(25) > ul > li:nth-child(5) > a',
         passANoteStudents: '#menu > li:nth-child(25) > ul > li:nth-child(6) > a',
         passANoteOptions: '#menu > li:nth-child(25) > ul > li:nth-child(7) > a',
+        //Email Menu
+        email: '#menu > li:nth-child(26)',
+        emailInbox: '#menu > li:nth-child(26) > ul > li:nth-child(1) > a',
+        emailFolders: '#menu > li:nth-child(26) > ul > li:nth-child(2) > a',
+        emailCompose: '#menu > li:nth-child(26) > ul > li:nth-child(3) > a',
+        emailPreferences: '#menu > li:nth-child(26) > ul > li:nth-child(4) > a',
+        emailAccounts: '#menu > li:nth-child(26) > ul > li:nth-child(5) > a',
 
         logout: '#menu > li:nth-child(22) > a',
         spiritStore: '#menu > li:nth-child(23) > a'
