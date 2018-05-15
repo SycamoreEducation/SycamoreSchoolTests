209b790a8af42aca60e83c5c8de5cc23f4908dbe
Ray Wagner <raywagner88@me.com>
2018-05-15 11:37:46 -0500

    Add accounting menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -181,6 +181,68 @@ module.exports = {
         classroomMgr: '#menu > li:nth-child(9) > ul > a:nth-child(15)',
         classroomMgrStudents: '#menu > li:nth-child(9) > ul > ul:nth-child(16) > li:nth-child(1) > a',
         classroomMgrColleges: '#menu > li:nth-child(9) > ul > ul:nth-child(16) > li:nth-child(2) > a',
+        //Financial Menu
+        financial: '#menu > li:nth-child(10)',
+        //Financial Accounting Manager
+        accountingMgr: '#menu > li:nth-child(10) > ul > a:nth-child(1)',
+        //Accounting Manager School
+        accountingMgrSchool: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > a:nth-child(1)',
+        accountingMgrSchoolChartOfAccounts: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(2) > li:nth-child(1) > a',
+        accountingMgrSchoolActivityLogs: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(2) > li:nth-child(2) > a',
+        //Accounting Manager Families 
+        accountingMgrFamilies: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > a:nth-child(3)',
+        accountingMgrFamiliesCurrent: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(4) > li:nth-child(1) > a',
+        accountingMgrFamiliesAccounts: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(4) > li:nth-child(2) > a',
+        accountingMgrFamiliesBillingSetup: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(4) > li:nth-child(3) > a',
+        accountingMgrFamiliesMonthlyCharges: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(4) > li:nth-child(4) > a',
+        accountingMgrFamiliesReceivePayment: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(4) > li:nth-child(5) > a',
+        accountingMgrFamiliesNotifications: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(4) > li:nth-child(6) > a',
+        accountingMgrFamiliesInvoices: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(4) > li:nth-child(7) > a',
+        //Accounting Manager Banking
+        accountingMgrBanking: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > a:nth-child(5)',
+        accountingMgrBankingDeposits: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(6) > li:nth-child(1) > a',
+        accountingMgrBankingRegisters: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(6) > li:nth-child(2) > a',
+        accountingMgrBankingJournal: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(6) > li:nth-child(3) > a',
+        //Accounting Manager Employees
+        accountingMgrEmployees: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > a:nth-child(7)',
+        //Accounting Manager Employees Payroll Menu
+        accountingMgrEmployeesPayroll: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(8) > a:nth-child(1)',
+        accountingMgrEmployeesPayrollByEmployee: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(8) > ul:nth-child(2) > li:nth-child(1) > a',
+        accountingMgrEmployeesPayrollByMonth: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(8) > ul:nth-child(2) > li:nth-child(2) > a',
+        //Accounting Manager Employees Expenses Menu
+        accountingMgrEmployeesExpenses: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(8) > a:nth-child(3)',
+        accountingMgrEmployeesExpensesSubmitted: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(8) > ul:nth-child(4) > li:nth-child(1) > a',
+        accountingMgrEmployeesExpensesApproved: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(8) > ul:nth-child(4) > li:nth-child(2) > a',
+        accountingMgrEmployeesExpensesPaid: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(8) > ul:nth-child(4) > li:nth-child(3) > a',
+        //Accounting Manager Services
+        accountingMgrServices: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > a:nth-child(9)'
+        //Accounting Manager Services Dwolla Menu 
+        accountingMgrServicesDwolla: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > a:nth-child(1)'
+        accountingMgrServicesDwollaTransactions: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(2) > li:nth-child(1) > a',
+        accountingMgrServicesDwollaConfiguration: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(2) > li:nth-child(2) > a',
+        //Accounting Manager Services Payjunction:
+        accountingMgrServicesPayJunction: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > a:nth-child(3)',
+        accountingMgrServicesPayJunctionInvoices: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(4) > li:nth-child(1) > a',
+        accountingMgrServicesPayJunctionPayments: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(4) > li:nth-child(2) > a',
+        accountingMgrServicesPayJunctionConvenienceFees: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(4) > li:nth-child(3) > a',
+        accountingMgrServicesPayJunctionConfiguration: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(4) > li:nth-child(4) > a',
+        //Accouting Manager Services Smart Tuition
+        accountingMgrServicesSmartTuition: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > a:nth-child(5)',
+        accountingMgrServicesSmartTuitionConfiguration: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(6) > li > a',
+        //Accounting Manager Services PayPal
+        accountingMgrServicesPayPal: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > a:nth-child(7)',
+        accountingMgrServicesPayPalOnlinePayments: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(8) > li:nth-child(1) > a'
+        accountingMgrServicesPayPalInvoices: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(8) > li:nth-child(2) > a',
+        accountingMgrServicesPayPalConfiguration: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(8) > li:nth-child(3) > a',
+        //Accounting Manager Services EFunds
+        accountingMgrServicesEFunds: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > a:nth-child(9)',
+        accountingMgrServicesEFundsPayments: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(10) > li:nth-child(1) > a',
+        accountingMgrServicesEFundsTransactions: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(10) > li:nth-child(2) > a',
+        accountingMgrServicesEFundsRemittance: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(10) > li:nth-child(3) > a',
+        accountingMgrServicesEFundsConfiguration: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > ul:nth-child(10) > ul:nth-child(10) > li:nth-child(4) > a',
+        //Accounting Manger Menu Items
+        accountingMgrConfiguration: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > li:nth-child(11) > a',
+        accountingMgrReports: '#menu > li:nth-child(10) > ul > ul:nth-child(2) > li:nth-child(12) > a',
 
         specialties: 'a[data-value=Specialties]',
         athleticDirector: '#menu > li:nth-child(8) > ul > a:nth-child(1)',
