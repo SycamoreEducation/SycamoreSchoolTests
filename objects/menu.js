1abdcd18b00428d3c34463122314b0fc8888ac0f
Ray Wagner <raywagner88@me.com>
2018-05-16 16:29:07 -0500

    Update my accounting menu test

--- objects/menu.js
+++ objects/menu.js
@@ -540,7 +540,7 @@ module.exports = {
         //My Organizer Menu Items
         myPassword: '#menu > li:nth-child(21) > ul > li:nth-child(12) > a',
         //My Accounting Menu
-        myAccounting: '#menu > li:nth-child(22)',
+        myAccounting: '#menu > li:nth-child(22) > a',
         myAccountingCafetieria: '#menu > li:nth-child(22) > ul > li:nth-child(1) > a',
         myAccountingExpenseReports: '#menu > li:nth-child(22) > ul > li:nth-child(2) > a',
         //My Resources Menu
