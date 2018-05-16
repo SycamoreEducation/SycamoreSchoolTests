354c8fbd66135db870879e1fc1fca6c81cc7b55d
Ray Wagner <raywagner88@me.com>
2018-05-16 14:51:09 -0500

    Update system support menu test

--- objects/menu.js
+++ objects/menu.js
@@ -479,7 +479,7 @@ module.exports = {
         sycamoreLeafNotes: '#menu > li:nth-child(15) > ul > li:nth-child(5) > a',
         releaseNotes: '#menu > li:nth-child(15) > ul > li:nth-child(6) > a',
         //System Support Menu
-        systemSupport: '#menu > li:nth-child(17)',
+        systemSupport: '#menu > li:nth-child(17) > a',
         openTickets: '#menu > li:nth-child(17) > ul > li:nth-child(1) > a',
         closedTickets: '#menu > li:nth-child(17) > ul > li:nth-child(2) > a',
         //Site Admin Menu
