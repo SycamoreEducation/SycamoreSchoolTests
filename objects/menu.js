ece13bc3bf6d83f55f0713556abea47fbe37b723
Ray Wagner <raywagner88@me.com>
2018-05-17 15:55:01 -0500

    Add administrative menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,56 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //Administrative Menu
+        administrative: '#menu > li:nth-child(8) > a',
+        //Administrative Back Office Mgr
+        backOfficeMgr: '#menu > li:nth-child(8) > ul > a:nth-child(1)',
+        //Back Office Mgr Students
+        backOfficeMgrStudents: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > a:nth-child(1)',
+        backOfficeMgrStudentsAll: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > ul:nth-child(2) > li:nth-child(1) > a',
+        backOfficeMgrStudentsCurrent: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > ul:nth-child(2) > li:nth-child(2) > a',
+        backOfficeMgrStudentsTypes: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > ul:nth-child(2) > li:nth-child(3) > a',
+        backOfficeMgrStudentsAdditionalFields: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > ul:nth-child(2) > li:nth-child(4) > a',
+        backOfficeMgrStudentsCategories: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > ul:nth-child(2) > li:nth-child(5) > a',
+        backOfficeMgrStudentsActivites: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > ul:nth-child(2) > li:nth-child(6) > a',
+        backOfficeMgrStudentsChecklists: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > ul:nth-child(2) > li:nth-child(7) > a',
+        //Back Office Mgr Families
+        backOfficeMgrFamilies: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > a:nth-child(3)',
+        backOfficeMgrFamiliesAll: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > ul:nth-child(4) > li:nth-child(1) > a',
+        backOfficeMgrFamiliesCurrent: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > ul:nth-child(4) > li:nth-child(2) > a',
+        backOfficeMgrFamiliesAdditionalFields: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > ul:nth-child(4) > li:nth-child(3) > a',
+        backOfficeMgrFamiliesNewFamily: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > ul:nth-child(4) > li:nth-child(4) > a',
+        //Back Office Mgr Menu Items
+        backOfficeReports: '#menu > li:nth-child(8) > ul > ul:nth-child(2) > li > a',
+        //Administrative Attendance
+        attendance: '#menu > li:nth-child(8) > ul > a:nth-child(3)',
+        //Attendance Menu Items
+        attendanceHomeroom: '#menu > li:nth-child(8) > ul > ul:nth-child(4) > li:nth-child(1) > a',
+        attendanceByPeriods: '#menu > li:nth-child(8) > ul > ul:nth-child(4) > li:nth-child(2) > a',
+        attendanceByStudent: '#menu > li:nth-child(8) > ul > ul:nth-child(4) > li:nth-child(3) > a',
+        attendanceByClassroom: '#menu > li:nth-child(8) > ul > ul:nth-child(4) > li:nth-child(4) > a',
+        attendanceByEmployee: '#menu > li:nth-child(8) > ul > ul:nth-child(4) > li:nth-child(5) > a',
+        attendanceDailyAbsences: '#menu > li:nth-child(8) > ul > ul:nth-child(4) > li:nth-child(6) > a',
+        attendanceCheckInOut: '#menu > li:nth-child(8) > ul > ul:nth-child(4) > li:nth-child(7) > a',
+        attendanceRecordTypes: '#menu > li:nth-child(8) > ul > ul:nth-child(4) > li:nth-child(8) > a',
+        attendanceScale: '#menu > li:nth-child(8) > ul > ul:nth-child(4) > li:nth-child(9) > a',
+        attendanceReports: '#menu > li:nth-child(8) > ul > ul:nth-child(4) > li:nth-child(10) > a',
+        //Administrative Administration
+        administration: '#menu > li:nth-child(8) > ul > a:nth-child(5)',
+        administrationSchoolYears: '#menu > li:nth-child(8) > ul > ul:nth-child(6) > li:nth-child(1) > a',
+        administrationSchoolPeriods: '#menu > li:nth-child(8) > ul > ul:nth-child(6) > li:nth-child(2) > a',
+        administrationGradeLevels: '#menu > li:nth-child(8) > ul > ul:nth-child(6) > li:nth-child(3) > a',
+        administrationCalendarOptions: '#menu > li:nth-child(8) > ul > ul:nth-child(6) > li:nth-child(4) > a',
+        administrationReportTemplate: '#menu > li:nth-child(8) > ul > ul:nth-child(6) > li:nth-child(5) > a',
+        administrationReportArchives: '#menu > li:nth-child(8) > ul > ul:nth-child(6) > li:nth-child(7) > a:nth-child(2)',
+        //Administartive Human Resources
+        humanReources: '#menu > li:nth-child(8) > ul > a:nth-child(7)',
+        humanResourcesEmployees: '#menu > li:nth-child(8) > ul > ul:nth-child(8) > li:nth-child(1) > a',
+        humanResourcesSubstitutes: '#menu > li:nth-child(8) > ul > ul:nth-child(8) > li:nth-child(2) > a',
+        humanResourcesDepartments: '#menu > li:nth-child(8) > ul > ul:nth-child(8) > li:nth-child(3) > a',
+        humanResourcesPositions: '#menu > li:nth-child(8) > ul > ul:nth-child(8) > li:nth-child(4) > a',
+        humanResourcesTypes: '#menu > li:nth-child(8) > ul > ul:nth-child(8) > li:nth-child(5) > a',
+        humanResourcesCategories: '#menu > li:nth-child(8) > ul > ul:nth-child(8) > li:nth-child(6) > a',
+        humanResourcesAdditionalFields: '#menu > li:nth-child(8) > ul > ul:nth-child(8) > li:nth-child(7) > a',
+        humanResourcesReports: '#menu > li:nth-child(8) > ul > ul:nth-child(8) > li:nth-child(8) > a',
     }
 };
