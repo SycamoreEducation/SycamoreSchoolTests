e5db8e8ba1467cb8cba8bd696e42805d7a2820e4
Ray Wagner <raywagner88@me.com>
2018-05-17 10:11:10 -0500

    Add front desk menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,11 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //Front Desk Menu
+        frontDesk: '#menu > li:nth-child(2) > a',
+        frontDeskFamilyDirectory: '#menu > li:nth-child(2) > ul > li:nth-child(1) > a',
+        frontDeskEmployeeDirectory: '#menu > li:nth-child(2) > ul > li:nth-child(2) > a',
+        frontDeskClassroomDirectory: '#menu > li:nth-child(2) > ul > li:nth-child(3) > a',
+        frontDeskDepartments: '#menu > li:nth-child(2) > ul > li:nth-child(4) > a',
+        frontDeskFacility: '#menu > li:nth-child(2) > ul > li:nth-child(5) > a',
     }
 };
