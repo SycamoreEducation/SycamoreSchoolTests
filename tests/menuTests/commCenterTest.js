5578a4ac9b2011e5479c61ec807b977c5a8e2df0
Ray Wagner <raywagner88@me.com>
2018-05-17 14:12:46 -0500

    Add comm center menu test

---
+++ tests/menuTests/commCenterTest.js
@@ -0,0 +1,43 @@
+module.exports = {
+    tags: ['commCenterTest', 'menuTests'],
+
+    'Comm Center Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin('superuser', 'guest', '1');
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@commCenter')
+            .assert.containsText('@commCenter', '\273 Comm Center')
+            .assert.attributeEquals('@commCenter', 'href', 'http://web/schoolmenu.php#')
+
+            .assert.hidden('@commCenterBlogs')
+            .assert.hidden('@commCenterBlogs')
+
+            .click('@commCenter')
+
+            .assert.visible('@commCenter')
+            .assert.visible('@commCenterBlogs')
+            .assert.visible('@commCenterDiscussions')
+
+            .assert.containsText('@commCenterBlogs', 'Blogs')
+            .assert.containsText('@commCenterDiscussions', 'Discussions')
+
+            .assert.attributeEquals('@commCenterBlogs', 'href', 'http://web/blogs.php')
+            .assert.attributeEquals('@commCenterDiscussions', 'href', 'http://web/forumboard.php?pid=0')
+
+            .click('@commCenter')
+
+            .assert.visible('@commCenter')
+
+            .assert.hidden('@commCenterBlogs')
+            .assert.hidden('@commCenterDiscussions')
+
+        client.frame(null);
+
+        client.end();
+    }
+};
