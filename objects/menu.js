b417d525ce53248902750792f93b956141cf3195
Ray Wagner <raywagner88@me.com>
2018-05-17 15:49:37 -0500

    Add communication menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,32 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //Communication Menu
+        communication: '#menu > li:nth-child(7) > a',
+        //Communicaton Homepage
+        communicationHomepage: '#menu > li:nth-child(7) > ul > a:nth-child(1)',
+        communicationHomepagePolling: '#menu > li:nth-child(7) > ul > ul:nth-child(2) > li:nth-child(1) > a',
+        communicationHomepageFlashNews: '#menu > li:nth-child(7) > ul > ul:nth-child(2) > li:nth-child(2) > a',
+        communicationHomepageSponsorAds: '#menu > li:nth-child(7) > ul > ul:nth-child(2) > li:nth-child(3) > a',
+        communicationHomepageRequest: '#menu > li:nth-child(7) > ul > ul:nth-child(2) > li:nth-child(4) > a',
+        communicationHomepageMsgOfTheDay: '#menu > li:nth-child(7) > ul > ul:nth-child(2) > li:nth-child(5) > a',
+        //Communication Batch Email
+        communicationBatchEmail: '#menu > li:nth-child(7) > ul > a:nth-child(3)',
+        communicationBatchEmailCompose: '#menu > li:nth-child(7) > ul > ul:nth-child(4) > li:nth-child(1) > a',
+        communicationBatchEmailDrafts: '#menu > li:nth-child(7) > ul > ul:nth-child(4) > li:nth-child(2) > a',
+        communicationBatchEmailScheduled: '#menu > li:nth-child(7) > ul > ul:nth-child(4) > li:nth-child(3) > a',
+        communicationBatchEmailSent: '#menu > li:nth-child(7) > ul > ul:nth-child(4) > li:nth-child(4) > a',
+        //Communication Text Messaging
+        communicationTextMessaging: '#menu > li:nth-child(7) > ul > a:nth-child(5)',
+        communicationTextMessagingBatchTextMessage: '#menu > li:nth-child(7) > ul > ul:nth-child(6) > li:nth-child(1) > a',
+        communicationTextMessagingMessageLogs: '#menu > li:nth-child(7) > ul > ul:nth-child(6) > li:nth-child(2) > a',
+        //Communication Menu Items
+        communicationTemplates: '#menu > li:nth-child(7) > ul > li:nth-child(7) > a',
+        communicationSafetyAlerts: '#menu > li:nth-child(7) > ul > li:nth-child(8) > a',
+        communicationMemoMaker: '#menu > li:nth-child(7) > ul > li:nth-child(9) > a',
+        communicationEVerification: '#menu > li:nth-child(7) > ul > li:nth-child(10) > a',
+        communicationChecklist: '#menu > li:nth-child(7) > ul > li:nth-child(11) > a',
+        communicationNotificationLists: '#menu > li:nth-child(7) > ul > li:nth-child(12) > a',
+        communicationConfiguration: '#menu > li:nth-child(7) > ul > li:nth-child(13) > a',
+        communicationReports: '#menu > li:nth-child(7) > ul > li:nth-child(14) > a',
     }
 };
