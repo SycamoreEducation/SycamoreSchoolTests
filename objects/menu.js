44b6425c649019ea5fe45244d84504b6e2337333
Ray Wagner <raywagner88@me.com>
2018-05-17 08:56:41 -0500

    Add school menu test

--- objects/menu.js
+++ objects/menu.js
@@ -1,12 +1,12 @@
 module.exports = {
     elements: {
         //School Menu
-        school: '#menu > li:nth-child(1)',
+        school: '#menu > li:nth-child(1) > a',
         schoolNews: '#menu > li:nth-child(1) > ul > li:nth-child(1) > a',
         schoolCalendar: '#menu > li:nth-child(1) > ul > li:nth-child(2) > a',
         schoolDocuments: '#menu > li:nth-child(1) > ul > li:nth-child(3) > a',
         schoolPhotoAlbums: '#menu > li:nth-child(1) > ul > li:nth-child(4) > a',
         schoolMultimedia: '#menu > li:nth-child(1) > ul > li:nth-child(5) > a',
-        schoolFarofites: '#menu > li:nth-child(1) > ul > li:nth-child(6) > a',
+        schoolFarvorites: '#menu > li:nth-child(1) > ul > li:nth-child(6) > a',
     }
 };
---
+++ tests/menuTests/schoolTest.js
@@ -0,0 +1,57 @@
+module.exports = {
+    tags: ['schoolTest', 'menuTests'],
+
+    'My Communication Menu Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.pause(2000);
+        client.page.menu()
+            .assert.visible('@school')
+            .assert.containsText('@school', '\273 School')
+            .assert.attributeEquals('@school', 'href', 'http://web/schoolmenu.php#')
+
+            .assert.hidden('@schoolNews')
+            .assert.hidden('@schoolCalendar')
+            .assert.hidden('@schoolDocuments')
+            .assert.hidden('@schoolPhotoAlbums')
+            .assert.hidden('@schoolMultimedia')
+            .assert.hidden('@schoolFarvorites')
+
+            .click('@school')
+            .assert.visible('@schoolNews')
+            .assert.visible('@schoolCalendar')
+            .assert.visible('@schoolDocuments')
+            .assert.visible('@schoolPhotoAlbums')
+            .assert.visible('@schoolMultimedia')
+            .assert.visible('@schoolFarvorites')
+            .assert.containsText('@schoolNews', 'News')
+            .assert.containsText('@schoolCalendar', 'Calendar')
+            .assert.containsText('@schoolDocuments', 'Documents')
+            .assert.containsText('@schoolPhotoAlbums', 'Photo Albums')
+            .assert.containsText('@schoolMultimedia', 'Multimedia')
+            .assert.containsText('@schoolFarvorites', 'Favorites')
+            .assert.attributeEquals('@schoolNews', 'href', 'http://web/news.php?classid=0')
+            .assert.attributeEquals('@schoolCalendar', 'href', 'http://web/calendar.php?classid=0')
+            .assert.attributeEquals('@schoolDocuments', 'href', 'http://web/explorer.php?classid=0')
+            .assert.attributeEquals('@schoolPhotoAlbums', 'href', 'http://web/photos.php?task=albums&classid=0')
+            .assert.attributeEquals('@schoolMultimedia', 'href', 'http://web/multimedia.php?classid=0')
+            .assert.attributeEquals('@schoolFarvorites', 'href', 'http://web/favorites.php?classid=0')
+
+            .click('@school')
+            .assert.hidden('@schoolNews')
+            .assert.hidden('@schoolCalendar')
+            .assert.hidden('@schoolDocuments')
+            .assert.hidden('@schoolPhotoAlbums')
+            .assert.hidden('@schoolMultimedia')
+            .assert.hidden('@schoolFarvorites')
+
+        client.frame(null);
+
+        client.end();
+    }
+};
