995b6f57cd654d4d8431199b01ba787321b3f7b2
Ray Wagner <raywagner88@me.com>
2018-05-17 10:48:27 -0500

    Add info center menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,13 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //Info Center Menu
+        infoCenter: '#menu > li:nth-child(3)',
+        infoCenterLibrary: '#menu > li:nth-child(3) > ul > li:nth-child(1) > a',
+        infoCenterCafeteria: '#menu > li:nth-child(3) > ul > li:nth-child(2) > a',
+        infoCenterOnlineAuctions: '#menu > li:nth-child(3) > ul > li:nth-child(3) > a',
+        infoCenterDatabases: '#menu > li:nth-child(3) > ul > li:nth-child(4) > a',
+        infoCenterOnlineForms: '#menu > li:nth-child(3) > ul > li:nth-child(5) > a',
+        infoCenterEventRegistration: '#menu > li:nth-child(3) > ul > li:nth-child(6) > a',
+        infoCenterKnowledgebase: '#menu > li:nth-child(3) > ul > li:nth-child(7) > a',
     }
 };
