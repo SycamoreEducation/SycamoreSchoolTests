ed03be99c05a0e6005ffb011415fad52932d157e
Ray Wagner <raywagner88@me.com>
2018-05-17 16:18:27 -0500

    Add addmissions menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,46 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //Addmissions Menu
+        addmissions: '#menu > li:nth-child(11)',
+        //Addmissions Inquiries
+        inquiries: '#menu > li:nth-child(11) > ul > a:nth-child(1)',
+        inquiresStudents: '#menu > li:nth-child(11) > ul > ul:nth-child(2) > li:nth-child(1) > a',
+        inquiriesFamilies: '#menu > li:nth-child(11) > ul > ul:nth-child(2) > li:nth-child(2) > a',
+        //Addmissions Enrollment
+        enrollment: '#menu > li:nth-child(11) > ul > a:nth-child(3)',
+        enrollmentStudent: '#menu > li:nth-child(11) > ul > ul:nth-child(4) > li:nth-child(1) > a',
+        enrollmentFamilies: '#menu > li:nth-child(11) > ul > ul:nth-child(4) > li:nth-child(2) > a',
+        //Class Manager
+        classMgr: '#menu > li:nth-child(11) > ul > a:nth-child(5)',
+        classMgrClasses: '#menu > li:nth-child(11) > ul > ul:nth-child(6) > li > a',
+        //Course Manager
+        courseMgr: '#menu > li:nth-child(11) > ul > a:nth-child(7)',
+        courseMgrCourses: '#menu > li:nth-child(11) > ul > ul:nth-child(8) > li:nth-child(1) > a',
+        courseMgrTeachers: '#menu > li:nth-child(11) > ul > ul:nth-child(8) > li:nth-child(2) > a',
+        courseMgrStudents: '#menu > li:nth-child(11) > ul > ul:nth-child(8) > li:nth-child(3) > a',
+        courseMgrFacility: '#menu > li:nth-child(11) > ul > ul:nth-child(8) > li:nth-child(4) > a',
+        courseMgrScheduler: '#menu > li:nth-child(11) > ul > ul:nth-child(8) > li:nth-child(5) > a',
+        courseMgrConfiguration: '#menu > li:nth-child(11) > ul > ul:nth-child(8) > li:nth-child(6) > a',
+        courseMgrReports: '#menu > li:nth-child(11) > ul > ul:nth-child(8) > li:nth-child(7) > a',
+        //Addmissions Communication
+        addmissionsCommunication: '#menu > li:nth-child(11) > ul > a:nth-child(9)',
+        addmissionsCommunicationBatchEmail: '#menu > li:nth-child(11) > ul > ul:nth-child(10) > li > a',
+        //Portal Manager
+        portalMgr: '#menu > li:nth-child(11) > ul > a:nth-child(11)',
+        portalMgrStudents: '#menu > li:nth-child(11) > ul > ul:nth-child(12) > li:nth-child(1) > a',
+        portalMgrFamilies: '#menu > li:nth-child(11) > ul > ul:nth-child(12) > li:nth-child(2) > a',
+        portalMgrApplications: '#menu > li:nth-child(11) > ul > ul:nth-child(12) > li:nth-child(3) > a',
+        portalMgrEssayQuestions: '#menu > li:nth-child(11) > ul > ul:nth-child(12) > li:nth-child(4) > a',
+        portalMgrRequestedDocs: '#menu > li:nth-child(11) > ul > ul:nth-child(12) > li:nth-child(5) > a',
+        //Addmissions Configuration
+        addmissionsConfiguration: '#menu > li:nth-child(11) > ul > a:nth-child(13)',
+        addmissionsConfigurationChecklist: '#menu > li:nth-child(11) > ul > ul:nth-child(14) > li:nth-child(1) > a',
+        addmissionsConfigurationDocuments: '#menu > li:nth-child(11) > ul > ul:nth-child(14) > li:nth-child(2) > a',
+        addmissionsConfigurationAgreements: '#menu > li:nth-child(11) > ul > ul:nth-child(14) > li:nth-child(3) > a',
+        addmissionsConfigurationTuitionPlans: '#menu > li:nth-child(11) > ul > ul:nth-child(14) > li:nth-child(4) > a',
+        addmissionsConfigurationEnrollStatus: '#menu > li:nth-child(11) > ul > ul:nth-child(14) > li:nth-child(5) > a',
+        addmissionsConfigurationSetup: '#menu > li:nth-child(11) > ul > ul:nth-child(14) > li:nth-child(6) > a',
+        //Addmissions Menu Items
+        addmissionsReports: '#menu > li:nth-child(11) > ul > li > a',
     }
 };
