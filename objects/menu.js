545c354a5d692a88820e4e1b249081942b211a0c
Ray Wagner <raywagner88@me.com>
2018-05-17 15:15:09 -0500

    Add address book menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,11 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //Address Book Menu
+        addressBook: '#menu > li:nth-child(6)',
+        addressBookByLastName: '#menu > li:nth-child(6) > ul > li:nth-child(1) > a',
+        addressBookByCompany: '#menu > li:nth-child(6) > ul > li:nth-child(2) > a',
+        addressBookCategories: '#menu > li:nth-child(6) > ul > li:nth-child(3) > a',
+        addressBookAdditionalFields: '#menu > li:nth-child(6) > ul > li:nth-child(4) > a',
+        addressBookReports: '#menu > li:nth-child(6) > ul > li:nth-child(5) > a',
     }
 };
