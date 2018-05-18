03aa78aa27838e20511734d6480109e2aba29637
Ray Wagner <raywagner88@me.com>
2018-05-18 11:00:32 -0500

    Add specialties menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,87 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //Specialties Menu
+        specialties: '#menu > li:nth-child(12) > a',
+        //Athletic Director
+        athleticDirector: '#menu > li:nth-child(12) > ul > a:nth-child(1)',
+        athleticDirectorSports: '#menu > li:nth-child(12) > ul > ul:nth-child(2) > li:nth-child(1) > a',
+        athleticDirectorTeams: '#menu > li:nth-child(12) > ul > ul:nth-child(2) > li:nth-child(2) > a',
+        athleticDirectorScoreboard: '#menu > li:nth-child(12) > ul > ul:nth-child(2) > li:nth-child(3) > a',
+        athleticDirectorEquipment: '#menu > li:nth-child(12) > ul > ul:nth-child(2) > li:nth-child(4) > a',
+        //NursesOffice
+        nursesOffice: '#menu > li:nth-child(12) > ul > a:nth-child(3)',
+        nursesOfficeDailyLogs: '#menu > li:nth-child(12) > ul > ul:nth-child(4) > li:nth-child(1) > a',
+        nursesOfficeStudents: '#menu > li:nth-child(12) > ul > ul:nth-child(4) > li:nth-child(2) > a',
+        nursesOfficeConfiguration: '#menu > li:nth-child(12) > ul > ul:nth-child(4) > a',
+        nursesOfficeConfigurationComplaints: '#menu > li:nth-child(12) > ul > ul:nth-child(4) > ul > li:nth-child(1) > a',
+        nursesOfficeConfigurationMedications: '#menu > li:nth-child(12) > ul > ul:nth-child(4) > ul > li:nth-child(2) > a',
+        nursesOfficeConfigurationImmunizations: '#menu > li:nth-child(12) > ul > ul:nth-child(4) > ul > li:nth-child(3) > a',
+        nursesOfficeReports: '#menu > li:nth-child(12) > ul > ul:nth-child(4) > li:nth-child(5) > a',
+        // P/T Conferences
+        ptConferences: '#menu > li:nth-child(12) > ul > a:nth-child(5)',
+        ptConferencesTeachers: '#menu > li:nth-child(12) > ul > ul:nth-child(6) > li:nth-child(1) > a',
+        ptConferencesByDate: '#menu > li:nth-child(12) > ul > ul:nth-child(6) > li:nth-child(2) > a',
+        ptConferencesByTeacher: '#menu > li:nth-child(12) > ul > ul:nth-child(6) > li:nth-child(3) > a',
+        ptConferencesConfiguration: '#menu > li:nth-child(12) > ul > ul:nth-child(6) > li:nth-child(4) > a',
+        // Discipline
+        discipline: '#menu > li:nth-child(12) > ul > a:nth-child(7)',
+        disciplineStudents: '#menu > li:nth-child(12) > ul > ul:nth-child(8) > li:nth-child(1) > a',
+        disciplineDailyLogs: '#menu > li:nth-child(12) > ul > ul:nth-child(8) > li:nth-child(2) > a',
+        disciplineDetentions: '#menu > li:nth-child(12) > ul > ul:nth-child(8) > li:nth-child(3) > a',
+        disciplineConfiguration: '#menu > li:nth-child(12) > ul > ul:nth-child(8) > li:nth-child(4) > a',
+        disciplineReports: '#menu > li:nth-child(12) > ul > ul:nth-child(8) > li:nth-child(5) > a',
+        //Service Manager
+        serviceMgr: '#menu > li:nth-child(12) > ul > a:nth-child(9)',
+        serviceMgrStudents: '#menu > li:nth-child(12) > ul > ul:nth-child(10) > li:nth-child(1) > a',
+        serviceMgrFamilies: '#menu > li:nth-child(12) > ul > ul:nth-child(10) > li:nth-child(2) > a',
+        serviceMgrDailyLogs: '#menu > li:nth-child(12) > ul > ul:nth-child(10) > li:nth-child(3) > a',
+        serviceMgrOpportunites: '#menu > li:nth-child(12) > ul > ul:nth-child(10) > li:nth-child(4) > a',
+        serviceMgrReports: '#menu > li:nth-child(12) > ul > ul:nth-child(10) > li:nth-child(5) > a',
+        //Connection Manager
+        connectionMgr: '#menu > li:nth-child(12) > ul > a:nth-child(11)',
+        connectionMgrStudents: '#menu > li:nth-child(12) > ul > ul:nth-child(12) > li:nth-child(1) > a',
+        connectionMgrDailyLogs: '#menu > li:nth-child(12) > ul > ul:nth-child(12) > li:nth-child(2) > a',
+        connectionMgrInitiator: '#menu > li:nth-child(12) > ul > ul:nth-child(12) > li:nth-child(3) > a',
+        connectionMgrReports: '#menu > li:nth-child(12) > ul > ul:nth-child(12) > li:nth-child(4) > a',
+        //Transportation Manager
+        tranportationMgr: '#menu > li:nth-child(12) > ul > a:nth-child(13)',
+        tranportationMgrBusRoutes: '#menu > li:nth-child(12) > ul > ul:nth-child(14) > li:nth-child(1) > a',
+        tranportationMgrFieldTrips: '#menu > li:nth-child(12) > ul > ul:nth-child(14) > li:nth-child(2) > a',
+        tranportationMgrCarpoolMgr: '#menu > li:nth-child(12) > ul > ul:nth-child(14) > li:nth-child(3) > a',
+        tranportationMgrParkingPasses: '#menu > li:nth-child(12) > ul > ul:nth-child(14) > li:nth-child(4) > a',
+        tranportationMgrReports: '#menu > li:nth-child(12) > ul > ul:nth-child(14) > li:nth-child(5) > a',
+        //Volunteer Manager
+        volunteerMgr: '#menu > li:nth-child(12) > ul > a:nth-child(15)',
+        volunteerMgrEvents: '#menu > li:nth-child(12) > ul > ul:nth-child(16) > li > a',
+        //Library Manager
+        libraryMgr: '#menu > li:nth-child(12) > ul > a:nth-child(17)',
+        libraryMgrBook: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > li:nth-child(1) > a',
+        libraryMgrStudents: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > li:nth-child(2) > a',
+        libraryMgrCheckIn: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > li:nth-child(3) > a',
+        libraryMgrCheckOut: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > li:nth-child(4) > a',
+        libraryMgrWhatsOut: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > li:nth-child(5) > a',
+        //Library Manager Books Menu
+        libraryMgrBooks: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > a:nth-child(6)',
+        libraryMgrBooksLibrary: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > ul:nth-child(7) > li:nth-child(1) > a',
+        libraryMgrBooksTextBooks: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > ul:nth-child(7) > li:nth-child(2) > a',
+        libraryMgrBooksInventoryLibrary: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > ul:nth-child(7) > li:nth-child(3) > a',
+        //Library Manager Search Menu
+        libraryMgrSearch: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > a:nth-child(8)',
+        libraryMgrSearchLocal: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > ul:nth-child(9) > li:nth-child(1) > a',
+        libraryMgrSearchGoogle: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > ul:nth-child(9) > li:nth-child(2) > a',
+        //Library Manager Configuration Menu
+        libraryMgrConfiguration: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > a:nth-child(10)',
+        libraryMgrConfigurationLocations: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > ul:nth-child(11) > li:nth-child(1) > a',
+        libraryMgrConfigurationImport: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > ul:nth-child(11) > li:nth-child(2) > a',
+        libraryMgrConfigurationSetup: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > ul:nth-child(11) > li:nth-child(3) > a',
+        //Library Manager Menu Items
+        libraryMgrReports: '#menu > li:nth-child(12) > ul > ul:nth-child(18) > li:nth-child(12) > a',
+        //Auction Manager
+        auctionMgr: '#menu > li:nth-child(12) > ul > a:nth-child(19)',
+        auctionMgrStaff: '#menu > li:nth-child(12) > ul > ul:nth-child(20) > li:nth-child(1) > a',
+        auctionMgrAuctions: '#menu > li:nth-child(12) > ul > ul:nth-child(20) > li:nth-child(2) > a',
+        auctionMgrAuctionItems: '#menu > li:nth-child(12) > ul > ul:nth-child(20) > li:nth-child(3) > a',
+        auctionMgrDonorCompanies: '#menu > li:nth-child(12) > ul > ul:nth-child(20) > li:nth-child(4) > a',
+        auctionMgrReports: '#menu > li:nth-child(12) > ul > ul:nth-child(20) > li:nth-child(5) > a',
     }
 };
