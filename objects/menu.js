e0c183469399d72d502ae2db6a165e04ae44c5be
Ray Wagner <raywagner88@me.com>
2018-05-18 11:27:06 -0500

    Add utilities menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,54 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //Utilities Menu
+        utilities: '#menu > li:nth-child(13) > a',
+        //Asset Manager 
+        assetMgr: '#menu > li:nth-child(13) > ul > a:nth-child(1)',
+        assetMgrInventory: '#menu > li:nth-child(13) > ul > ul:nth-child(2) > li:nth-child(1) > a',
+        assetMgrTypes: '#menu > li:nth-child(13) > ul > ul:nth-child(2) > li:nth-child(2) > a',
+        assetMgrAdditionalFields: '#menu > li:nth-child(13) > ul > ul:nth-child(2) > li:nth-child(3) > a',
+        assetMgrReports: '#menu > li:nth-child(13) > ul > ul:nth-child(2) > li:nth-child(4) > a',
+        //Event Manager
+        eventMgr: '#menu > li:nth-child(13) > ul > a:nth-child(3)',
+        eventMgrManagedEvents: '#menu > li:nth-child(13) > ul > ul:nth-child(4) > li:nth-child(1) > a',
+        eventMgrRegistration: '#menu > li:nth-child(13) > ul > ul:nth-child(4) > li:nth-child(2) > a',
+        eventMgrParticipants: '#menu > li:nth-child(13) > ul > ul:nth-child(4) > li:nth-child(3) > a',
+        eventMgrReports: '#menu > li:nth-child(13) > ul > ul:nth-child(4) > li:nth-child(4) > a',
+        //Facility Manager
+        facilityMgr: '#menu > li:nth-child(13) > ul > a:nth-child(5)',
+        facilityMgrLocations: '#menu > li:nth-child(13) > ul > ul:nth-child(6) > li:nth-child(1) > a',
+        facilityMgrBuildings: '#menu > li:nth-child(13) > ul > ul:nth-child(6) > li:nth-child(2) > a',
+        facilityMgrRooms: '#menu > li:nth-child(13) > ul > ul:nth-child(6) > li:nth-child(3) > a',
+        facilityMgrPlanner: '#menu > li:nth-child(13) > ul > ul:nth-child(6) > li:nth-child(4) > a',
+        facilityMgrActivities: '#menu > li:nth-child(13) > ul > ul:nth-child(6) > li:nth-child(5) > a',
+        facilityMgrSearch: '#menu > li:nth-child(13) > ul > ul:nth-child(6) > li:nth-child(6) > a',
+        //Timecard Manager
+        timecardMgr: '#menu > li:nth-child(13) > ul > a:nth-child(7)',
+        timecardMgrByDay: '#menu > li:nth-child(13) > ul > ul:nth-child(8) > li:nth-child(1) > a',
+        timecardMgrByPeriod: '#menu > li:nth-child(13) > ul > ul:nth-child(8) > li:nth-child(2) > a',
+        timecardMgrByEmployee: '#menu > li:nth-child(13) > ul > ul:nth-child(8) > li:nth-child(3) > a',
+        timecardMgrRequests: '#menu > li:nth-child(13) > ul > ul:nth-child(8) > li:nth-child(4) > a',
+        timecardMgrTasks: '#menu > li:nth-child(13) > ul > ul:nth-child(8) > li:nth-child(5) > a',
+        timecardMgrConfiguration: '#menu > li:nth-child(13) > ul > ul:nth-child(8) > li:nth-child(6) > a',
+        timecardMgrReports: '#menu > li:nth-child(13) > ul > ul:nth-child(8) > li:nth-child(7) > a',
+        //Workflow Manager
+        workflowMgr: '#menu > li:nth-child(13) > ul > a:nth-child(9)',
+        workflowMgrByStatus: '#menu > li:nth-child(13) > ul > ul:nth-child(10) > li:nth-child(1) > a',
+        workflowMgrByAuthor: '#menu > li:nth-child(13) > ul > ul:nth-child(10) > li:nth-child(2) > a',
+        workflowMgrByCategory: '#menu > li:nth-child(13) > ul > ul:nth-child(10) > li:nth-child(3) > a',
+        workflowMgrByClassroom: '#menu > li:nth-child(13) > ul > ul:nth-child(10) > li:nth-child(4) > a',
+        workflowMgrCategories: '#menu > li:nth-child(13) > ul > ul:nth-child(10) > li:nth-child(5) > a',
+        //Database Manager
+        databaseMgr: '#menu > li:nth-child(13) > ul > a:nth-child(11)',
+        databaseMgrPublicTables: '#menu > li:nth-child(13) > ul > ul:nth-child(12) > li:nth-child(1) > a',
+        databaseMgrPrivateTables: '#menu > li:nth-child(13) > ul > ul:nth-child(12) > li:nth-child(2) > a',
+        //Online Form Manager
+        onlineFormMgr: '#menu > li:nth-child(13) > ul > a:nth-child(13)',
+        onlineFormMgrActive: '#menu > li:nth-child(13) > ul > ul:nth-child(14) > li:nth-child(1) > a',
+        onlineFormMgrNonActive: '#menu > li:nth-child(13) > ul > ul:nth-child(14) > li:nth-child(2) > a',
+        //Knowledge Base
+        knowledgeBase: '#menu > li:nth-child(13) > ul > a:nth-child(15)',
+        knowledgeBasePublished: '#menu > li:nth-child(13) > ul > ul:nth-child(16) > li:nth-child(1) > a',
+        knowledgeBasePending: '#menu > li:nth-child(13) > ul > ul:nth-child(16) > li:nth-child(2) > a',
     }
 };
