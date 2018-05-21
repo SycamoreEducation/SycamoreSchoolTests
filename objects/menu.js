23d565dd205e3b9ca3958653365b3f1f5bcfe15f
Ray Wagner <raywagner88@me.com>
2018-05-21 09:30:14 -0500

    Add my school menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,18 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //My School Menu
+        mySchool: '#menu > li:nth-child(20) > a',
+        myClasses: '#menu > li:nth-child(20) > ul > li:nth-child(1) > a',
+        myStudents: '#menu > li:nth-child(20) > ul > li:nth-child(2) > a',
+        myServiceLogs: '#menu > li:nth-child(20) > ul > li:nth-child(3) > a',
+        myLessonPlans: '#menu > li:nth-child(20) > ul > li:nth-child(4) > a',
+        myDisciplineLogs: '#menu > li:nth-child(20) > ul > li:nth-child(5) > a',
+        myConnectionLogs: '#menu > li:nth-child(20) > ul > li:nth-child(6) > a',
+        myNotifications: '#menu > li:nth-child(20) > ul > li:nth-child(7) > a',
+        myWorkFlow: '#menu > li:nth-child(20) > ul > li:nth-child(8) > a',
+        myTimecard: '#menu > li:nth-child(20) > ul > li:nth-child(9) > a',
+        myEmployees: '#menu > li:nth-child(20) > ul > li:nth-child(10) > a',
+        staffDiscussions: '#menu > li:nth-child(20) > ul > li:nth-child(11) > a',
+        schoolSchedule: '#menu > li:nth-child(20) > ul > li:nth-child(12) > a',
     }
 };
