c2d67edc1bce48afbbc990762f1c13a642edd145
Ray Wagner <raywagner88@me.com>
2018-05-21 09:50:13 -0500

    Add my resources menu test

---
+++ tests/menuTests/myResourcesTest.js
@@ -0,0 +1,51 @@
+module.exports = {
+    tags: ['myResourcesTest', 'menuTests'],
+
+    'My Resources Menu Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@myResources')
+            .assert.containsText('@myResources', '\273 My Resources')
+            .assert.attributeEquals('@myResources', 'href', 'http://web/schoolmenu.php#')
+
+            .assert.hidden('@myResourcesNews')
+            .assert.hidden('@myResourcesPhotos')
+            .assert.hidden('@myResourcesDocuments')
+            .assert.hidden('@myResourcesOnlineTests')
+            .assert.hidden('@myResourcesGradeBook')
+
+            .click('@myResources')
+            .assert.visible('@myResourcesNews')
+            .assert.visible('@myResourcesPhotos')
+            .assert.visible('@myResourcesDocuments')
+            .assert.visible('@myResourcesOnlineTests')
+            .assert.visible('@myResourcesGradeBook')
+            .assert.containsText('@myResourcesNews', 'News')
+            .assert.containsText('@myResourcesPhotos', 'Photos')
+            .assert.containsText('@myResourcesDocuments', 'Documents')
+            .assert.containsText('@myResourcesOnlineTests', 'Online Tests')
+            .assert.containsText('@myResourcesGradeBook', 'Grade Book')
+            .assert.attributeEquals('@myResourcesNews', 'href', 'http://web/cloudnews.php')
+            .assert.attributeEquals('@myResourcesPhotos', 'href', 'http://web/cloudphotos.php')
+            .assert.attributeEquals('@myResourcesDocuments', 'href', 'http://web/clouddocuments.php')
+            .assert.attributeEquals('@myResourcesOnlineTests', 'href', 'http://web/myonlinetests.php')
+            .assert.attributeEquals('@myResourcesGradeBook', 'href', 'http://web/mygradebooks.php')
+
+            .click('@myResources')
+            .assert.hidden('@myResourcesNews')
+            .assert.hidden('@myResourcesPhotos')
+            .assert.hidden('@myResourcesDocuments')
+            .assert.hidden('@myResourcesOnlineTests')
+            .assert.hidden('@myResourcesGradeBook')
+
+        client.frame(null);
+
+        client.end();
+    }
+};
