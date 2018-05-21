a72fbeca247b49a440dab6ef7ec14402f65923fd
Ray Wagner <raywagner88@me.com>
2018-05-21 09:48:30 -0500

    Add my resource menu test

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,11 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //My Resources Menu
+        myResources: '#menu > li:nth-child(23) > a',
+        myResourcesNews: '#menu > li:nth-child(23) > ul > li:nth-child(1) > a',
+        myResourcesPhotos: '#menu > li:nth-child(23) > ul > li:nth-child(2) > a',
+        myResourcesDocuments: '#menu > li:nth-child(23) > ul > li:nth-child(3) > a',
+        myResourcesOnlineTests: '#menu > li:nth-child(23) > ul > li:nth-child(4) > a',
+        myResourcesGradeBook: '#menu > li:nth-child(23) > ul > li:nth-child(5) > a',
     }
 };
