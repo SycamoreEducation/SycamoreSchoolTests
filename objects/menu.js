24fa9296c400b6d44da09807665eed29cf6c6f90
Ray Wagner <raywagner88@me.com>
2018-05-21 09:36:31 -0500

    Add my organizer menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,24 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //My Organizer Menu
+        myOrganizer: '#menu > li:nth-child(21) > a',
+        myDesktop: '#menu > li:nth-child(21) > ul > li:nth-child(1) > a',
+        myTasks: '#menu > li:nth-child(21) > ul > li:nth-child(2) > a',
+        myCalendar: '#menu > li:nth-child(21) > ul > li:nth-child(3) > a',
+        myDocuments: '#menu > li:nth-child(21) > ul > li:nth-child(4) > a',
+        myAddressBook: '#menu > li:nth-child(21) > ul > li:nth-child(5) > a',
+        myPhotoAlbums: '#menu > li:nth-child(21) > ul > li:nth-child(6) > a',
+        myMemos: '#menu > li:nth-child(21) > ul > li:nth-child(7) > a',
+        myFavoriteLinks: '#menu > li:nth-child(21) > ul > li:nth-child(8) > a',
+        myDailyJournal: '#menu > li:nth-child(21) > ul > li:nth-child(9) > a',
+        //My Utilties Menu
+        myUtilities: '#menu > li:nth-child(21) > ul > a',
+        myUtilitiesOptions: '#menu > li:nth-child(21) > ul > ul > li:nth-child(1) > a',
+        myUtilitiesCategories: '#menu > li:nth-child(21) > ul > ul > li:nth-child(2) > a',
+        myUtilitiesImport: '#menu > li:nth-child(21) > ul > ul > li:nth-child(3) > a',
+        myUtilitiesApplications: '#menu > li:nth-child(21) > ul > ul > li:nth-child(4) > a',
+        myUtilitiesWebHooks: '#menu > li:nth-child(21) > ul > ul > li:nth-child(5) > a',
+        //My Organizer Menu Items
+        myPassword: '#menu > li:nth-child(21) > ul > li:nth-child(12) > a',
     }
 };
