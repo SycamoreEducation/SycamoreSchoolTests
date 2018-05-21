debdb82aa7ddaeba9c6e654911119e3bda7074fc
Ray Wagner <raywagner88@me.com>
2018-05-21 09:11:25 -0500

    Add sytem help menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,12 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //System Help Menu
+        systemHelp: '#menu > li:nth-child(15) > a',
+        welcomeAboard: '#menu > li:nth-child(15) > ul > li:nth-child(1) > a',
+        onlineHelp: '#menu > li:nth-child(15) > ul > li:nth-child(2) > a',
+        trainingVideos: '#menu > li:nth-child(15) > ul > li:nth-child(3) > a',
+        webinarTraining: '#menu > li:nth-child(15) > ul > li:nth-child(4) > a',
+        sycamoreLeafNotes: '#menu > li:nth-child(15) > ul > li:nth-child(5) > a',
+        releaseNotes: '#menu > li:nth-child(15) > ul > li:nth-child(6) > a',
     }
 };
