9e52293990149e57b55b963ac653f9e4362db40d
Ray Wagner <raywagner88@me.com>
2018-05-21 10:11:18 -0500

    Add logout menu test

---
+++ tests/menuTests/logoutTest.js
@@ -0,0 +1,23 @@
+module.exports = {
+    tags: ['logoutTest', 'menuTests'],
+
+    'Logout Menu Test': (client) => {
+
+        var page = client.page.login();
+
+        var username = 'superuser';
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@logout')
+            .assert.containsText('@logout', 'Logout (' + username + ')')
+            .assert.attributeEquals('@logout', 'href', 'http://web/logout.php')
+
+        client.frame(null);
+
+        client.end();
+    }
+};
