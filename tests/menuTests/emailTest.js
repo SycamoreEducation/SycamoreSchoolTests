3d68342387948aab8a4234d44896bdfef55642c3
Ray Wagner <raywagner88@me.com>
2018-05-21 10:07:20 -0500

    Add email menu test

---
+++ tests/menuTests/emailTest.js
@@ -0,0 +1,51 @@
+module.exports = {
+    tags: ['emailTest', 'menuTests'],
+
+    'Email Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@email')
+            .assert.containsText('@email', '\273 Email')
+            .assert.attributeEquals('@email', 'href', 'http://web/schoolmenu.php#')
+
+            .assert.hidden('@emailInbox')
+            .assert.hidden('@emailFolders')
+            .assert.hidden('@emailCompose')
+            .assert.hidden('@emailPreferences')
+            .assert.hidden('@emailAccounts')
+
+            .click('@email')
+            .assert.visible('@emailInbox')
+            .assert.visible('@emailFolders')
+            .assert.visible('@emailCompose')
+            .assert.visible('@emailPreferences')
+            .assert.visible('@emailAccounts')
+            .assert.containsText('@emailInbox', 'Inbox')
+            .assert.containsText('@emailFolders', 'Folders')
+            .assert.containsText('@emailCompose', 'Compose')
+            .assert.containsText('@emailPreferences', 'Preferences')
+            .assert.containsText('@emailAccounts', 'Accounts')
+            .assert.attributeEquals('@emailInbox', 'href', 'http://web/rclogin.php?_task=mail')
+            .assert.attributeEquals('@emailFolders', 'href', 'http://web/rclogin.php?_task=settings&_action=folders')
+            .assert.attributeEquals('@emailCompose', 'href', 'http://web/rclogin.php?_task=mail&_action=compose')
+            .assert.attributeEquals('@emailPreferences', 'href', 'http://web/rclogin.php?_task=settings')
+            .assert.attributeEquals('@emailAccounts', 'href', 'http://web/rcaccounts.php')
+
+            .click('@email')
+            .assert.hidden('@emailInbox')
+            .assert.hidden('@emailFolders')
+            .assert.hidden('@emailCompose')
+            .assert.hidden('@emailPreferences')
+            .assert.hidden('@emailAccounts')
+
+        client.frame(null);
+
+        client.end();
+    }
+};
