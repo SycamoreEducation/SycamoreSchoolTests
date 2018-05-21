b4707fc0dedc235a1df23616eb7dbbaffa3afa55
Ray Wagner <raywagner88@me.com>
2018-05-21 09:20:35 -0500

    Add system support menu test

---
+++ tests/menuTests/systemSupportTest.js
@@ -0,0 +1,39 @@
+module.exports = {
+    tags: ['systemSupportTest', 'menuTests'],
+
+    'System Support Menu Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@systemSupport')
+            .assert.containsText('@systemSupport', '\273 System Support')
+            .assert.attributeEquals('@systemSupport', 'href', 'http://web/schoolmenu.php#')
+
+            .assert.hidden('@openTickets')
+            .assert.hidden('@closedTickets')
+
+            .click('@systemSupport')
+            .assert.visible('@openTickets')
+            .assert.visible('@closedTickets')
+            .assert.containsText('@openTickets', 'Open Tickets')
+            .assert.containsText('@closedTickets', 'Closed Tickets')
+            .assert.attributeEquals('@openTickets', 'href', 'http://web/support.php?type=open')
+            .assert.attributeEquals('@closedTickets', 'href', 'http://web/support.php?type=closed')
+
+            .click('@systemSupport')
+
+            .assert.visible('@systemSupport')
+
+            .assert.hidden('@openTickets')
+            .assert.hidden('@closedTickets')
+
+        client.frame(null);
+
+        client.end();
+    }
+};
