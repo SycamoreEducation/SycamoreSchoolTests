f42779c986bc86642b8137641fae8473b9bfb01e
Ray Wagner <raywagner88@me.com>
2018-05-21 09:23:22 -0500

    Add site admin menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,27 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //Site Admin Menu
+        siteAdmin: '#menu > li:nth-child(18) > a',
+        siteAdminInformation: '#menu > li:nth-child(18) > ul > li:nth-child(1) > a',
+        siteAdminConfiguration: '#menu > li:nth-child(18) > ul > li:nth-child(2) > a',
+        siteAdminSubscription: '#menu > li:nth-child(18) > ul > li:nth-child(3) > a',
+        siteAdminPermissions: '#menu > li:nth-child(18) > ul > li:nth-child(4) > a',
+        siteAdminEmailAccounts: '#menu > li:nth-child(18) > ul > li:nth-child(5) > a',
+        siteAdminYearEndCloseout: '#menu > li:nth-child(18) > ul > li:nth-child(6) > a',
+        //System Logs
+        systemLogs: '#menu > li:nth-child(18) > ul > a:nth-child(7)',
+        systemLogsSiteUsage: '#menu > li:nth-child(18) > ul > ul:nth-child(8) > li:nth-child(1) > a',
+        systemLogsDailyOverview: '#menu > li:nth-child(18) > ul > ul:nth-child(8) > li:nth-child(2) > a',
+        systmeLogsSecurityLogs: '#menu > li:nth-child(18) > ul > ul:nth-child(8) > li:nth-child(3) > a',
+        //Connections
+        connections: '#menu > li:nth-child(18) > ul > a:nth-child(9)',
+        connectionsIntegrations: '#menu > li:nth-child(18) > ul > ul:nth-child(10) > li:nth-child(1) > a',
+        connectionsWebHooks: '#menu > li:nth-child(18) > ul > ul:nth-child(10) > li:nth-child(2) > a',
+        //Import Data
+        importData: '#menu > li:nth-child(18) > ul > a:nth-child(11)',
+        importDataMigrationWizard: '#menu > li:nth-child(18) > ul > ul:nth-child(12) > li:nth-child(1) > a',
+        importDataStudentPhotos: '#menu > li:nth-child(18) > ul > ul:nth-child(12) > li:nth-child(2) > a',
+        importDataEmployeePhotos: '#menu > li:nth-child(18) > ul > ul:nth-child(12) > li:nth-child(3) > a',
+        importDataMiscellaneous: '#menu > li:nth-child(18) > ul > ul:nth-child(12) > li:nth-child(4) > a',
     }
 };
