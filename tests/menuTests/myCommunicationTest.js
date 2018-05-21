955b8d36253af7278ade5a54c66500528f674996
Ray Wagner <raywagner88@me.com>
2018-05-21 09:53:38 -0500

    Add my communication menu test

---
+++ tests/menuTests/myCommunicationTest.js
@@ -0,0 +1,36 @@
+module.exports = {
+    tags: ['myCommunicationTest', 'menuTests'],
+
+    'My Communication Menu Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@myCommunication')
+            .assert.containsText('@myCommunication', '\273 My Communication')
+            .assert.attributeEquals('@myCommunication', 'href', 'http://web/schoolmenu.php#')
+
+            .assert.hidden('@myBatchEmail')
+            .assert.hidden('@myBlogs')
+
+            .click('@myCommunication')
+            .assert.visible('@myBatchEmail')
+            .assert.visible('@myBlogs')
+            .assert.containsText('@myBatchEmail', 'My Batch Email')
+            .assert.containsText('@myBlogs', 'My Blogs')
+            .assert.attributeEquals('@myBatchEmail', 'href', 'http://web/batchemail3.php?classid=-1')
+            .assert.attributeEquals('@myBlogs', 'href', 'http://web/blogs.php?task=mine')
+
+            .click('@myCommunication')
+            .assert.hidden('@myBatchEmail')
+            .assert.hidden('@myBlogs')
+
+        client.frame(null);
+
+        client.end();
+    }
+};
