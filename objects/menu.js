0a700f9d5652477b77c912340f33b4f5a28b2d21
Ray Wagner <raywagner88@me.com>
2018-05-21 09:59:21 -0500

    Add email menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,11 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //Email Menu
+        email: '#menu > li:nth-child(26)',
+        emailInbox: '#menu > li:nth-child(26) > ul > li:nth-child(1) > a',
+        emailFolders: '#menu > li:nth-child(26) > ul > li:nth-child(2) > a',
+        emailCompose: '#menu > li:nth-child(26) > ul > li:nth-child(3) > a',
+        emailPreferences: '#menu > li:nth-child(26) > ul > li:nth-child(4) > a',
+        emailAccounts: '#menu > li:nth-child(26) > ul > li:nth-child(5) > a',
     }
 };
