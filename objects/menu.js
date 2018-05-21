274581c85b7d362be15f9d886dced58a9e5132b0
Ray Wagner <raywagner88@me.com>
2018-05-21 09:56:32 -0500

    Add pass a note menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,13 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //Pass-A-Note Menu
+        passANote: '#menu > li:nth-child(25) > a',
+        passANoteInbox: '#menu > li:nth-child(25) > ul > li:nth-child(1) > a',
+        passANoteOutbox: '#menu > li:nth-child(25) > ul > li:nth-child(2) > a',
+        passANoteEmployees: '#menu > li:nth-child(25) > ul > li:nth-child(3) > a',
+        passANoteAssociates: '#menu > li:nth-child(25) > ul > li:nth-child(4) > a',
+        passANoteFamilies: '#menu > li:nth-child(25) > ul > li:nth-child(5) > a',
+        passANoteStudents: '#menu > li:nth-child(25) > ul > li:nth-child(6) > a',
+        passANoteOptions: '#menu > li:nth-child(25) > ul > li:nth-child(7) > a',
     }
 };
