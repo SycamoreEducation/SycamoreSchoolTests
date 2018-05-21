d43de2bbac5b0481e5acdf5df3d45f09dbc1b56d
Ray Wagner <raywagner88@me.com>
2018-05-21 09:51:48 -0500

    Add my communication menu objects

--- objects/menu.js
+++ objects/menu.js
@@ -1,7 +1,8 @@
 module.exports = {
     elements: {
-        home: {
-            selector: 'ul.breadcrumb li.active a'
-        }
+        //My Communications Menu
+        myCommunication: '#menu > li:nth-child(24) > a',
+        myBatchEmail: '#menu > li:nth-child(24) > ul > li:nth-child(1) > a',
+        myBlogs: '#menu > li:nth-child(24) > ul > li:nth-child(2) > a',
     }
 };
