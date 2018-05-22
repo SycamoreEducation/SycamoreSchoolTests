b6d1996ef589de622e8fd4026e9937b30558d00b
Ray Wagner <raywagner88@me.com>
2018-05-22 08:59:39 -0500

    Add news - current tab objects

--- objects/news.js
+++ objects/news.js
@@ -4,6 +4,20 @@ module.exports = {
         return this.api.launchUrl + '/news.php?classid=0';
     },
     elements: {
-
+        breadcrumb: 'body > ul.breadcrumb',
+        currentTab: 'body > ul.nav.nav-tabs > li:nth-child(1) > a',
+        archiveTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
+        searchTab: 'body > ul.nav.nav-tabs > li:nth-child(3) > a',
+        newArticleBtn: 'body > ul.nav.nav-tabs > input',
+        //Current Tab Elements
+        browse: 'body > div.tab-content > form > div > span.se-font-small > a',
+        list: 'body > div.tab-content > form > div > span.se-font-small > a',
+        resultMsg: 'body > div.tab-content > form > div > span.se-font.se-bold',
+        checkbox: 'body > div.tab-content > form > table > tbody > tr > td.setChecked > a',
+        date: 'body > div.tab-content > form > table > tbody > tr > td:nth-child(2)',
+        author: 'body > div.tab-content > form > table > tbody > tr > td:nth-child(3)',
+        headline: 'body > div.tab-content > form > table > tbody > tr > td:nth-child(4)',
+        archiveBtn: 'body > div.tab-content > form > input[type="submit"]:nth-child(8)',
+        deleteBtn: 'body > div.tab-content > form > input.btn.btn-mini.pull-right.btn-danger',
     }
 };
