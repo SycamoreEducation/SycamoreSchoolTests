b74145a2ca03561d2ea168f0d362378d4a0261b8
Ray Wagner <raywagner88@me.com>
2018-05-22 11:10:05 -0500

    Add news search tab test

---
+++ tests/schoolTests/newsTests/newsSearch.js
@@ -0,0 +1,54 @@
+module.exports = {
+    tags: ['schoolTests', 'newsTests', 'newsSearchTabTest'],
+
+    'School News Current Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@school')
+            .click('@school')
+            .assert.visible('@schoolNews')
+            .click('@schoolNews')
+        client.frame(null);
+
+        client.frame(2);
+        client.pause(2000)
+        client.page.news()
+
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 'School/News/ Browse')
+
+            .click('@searchTab')
+
+            .assert.visible('@searchCurrentTab')
+            .assert.containsText('@searchCurrentTab', 'Current')
+            .assert.attributeEquals('@searchCurrentTab', 'href', 'http://web/news.php?type=2&classid=0&tab=0')
+
+            .assert.visible('@searchArchivetab')
+            .assert.containsText('@searchArchivetab', 'Archive')
+            .assert.attributeEquals('@searchArchivetab', 'href', 'http://web/news.php?type=3&classid=0&tab=1')
+
+            .assert.visible('@searchSearchTab')
+            .assert.containsText('@searchSearchTab', 'Search')
+
+            .assert.visible('@searchField')
+
+            .assert.visible('@searchBtn')
+            .assert.attributeEquals('@searchBtn', 'value', 'Search')
+
+            .assert.visible('@searchDate')
+            .assert.containsText('@searchDate', 'Date')
+
+            .assert.visible('@searchHeadline')
+            .assert.containsText('@searchHeadline', 'Headline')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
