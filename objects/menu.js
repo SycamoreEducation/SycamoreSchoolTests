7f831d24f31fbd7c83ae019aba1bc7a1c1d6e398
Ray Wagner <raywagner88@me.com>
2018-05-23 09:12:43 -0500

    Remove trailing comma

--- objects/menu.js
+++ objects/menu.js
@@ -14,6 +14,6 @@ module.exports = {
         frontDeskEmployeeDirectory: '#menu > li:nth-child(2) > ul > li:nth-child(2) > a',
         frontDeskClassroomDirectory: '#menu > li:nth-child(2) > ul > li:nth-child(3) > a',
         frontDeskDepartments: '#menu > li:nth-child(2) > ul > li:nth-child(4) > a',
-        frontDeskFacility: '#menu > li:nth-child(2) > ul > li:nth-child(5) > a',
+        frontDeskFacility: '#menu > li:nth-child(2) > ul > li:nth-child(5) > a'
     }
 };
