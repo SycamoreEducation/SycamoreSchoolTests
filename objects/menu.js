4e05e59b2896f9f89e5a21a13aeb4a91935945fe
Ray Wagner <raywagner88@me.com>
2018-06-04 13:09:25 -0500

    Update mispelled menu object property

--- objects/menu.js
+++ objects/menu.js
@@ -7,7 +7,7 @@ module.exports = {
         schoolDocuments: '#menu > li:nth-child(1) > ul > li:nth-child(3) > a',
         schoolPhotoAlbums: '#menu > li:nth-child(1) > ul > li:nth-child(4) > a',
         schoolMultimedia: '#menu > li:nth-child(1) > ul > li:nth-child(5) > a',
-        schoolFarvorites: '#menu > li:nth-child(1) > ul > li:nth-child(6) > a',
+        schoolFavorites: '#menu > li:nth-child(1) > ul > li:nth-child(6) > a',
         //Front Desk Menu
         frontDesk: '#menu > li:nth-child(2) > a',
         frontDeskFamilyDirectory: '#menu > li:nth-child(2) > ul > li:nth-child(1) > a',
