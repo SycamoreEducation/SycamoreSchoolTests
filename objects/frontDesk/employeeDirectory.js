706528c09dc2a301095200e101a7ebeee61cbe36
Ray Wagner <raywagner88@me.com>
2018-06-07 14:50:39 -0500

    Update object for test

--- objects/frontDesk/employeeDirectory.js
+++ objects/frontDesk/employeeDirectory.js
@@ -10,6 +10,11 @@ module.exports = {
         tomName: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(1)',
         tomNote: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(5) > font > a',
         johnName: 'body > div.tab-content > table > tbody > tr:nth-child(3) > td:nth-child(1)',
-        johnNote: 'body > div.tab-content > table > tbody > tr:nth-child(3) > td:nth-child(5) > font > a'
+        johnNote: 'body > div.tab-content > table > tbody > tr:nth-child(3) > td:nth-child(5) > font > a',
+        // Photos Tab Elements
+        tomPhoto: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(1) > table > tbody > tr:nth-child(1) > td > a',
+        tomCaption: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(1) > table > tbody > tr:nth-child(2) > td',
+        johnPhoto: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr:nth-child(1) > td > a',
+        johnCaption: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr:nth-child(2) > td'
     }
 };
