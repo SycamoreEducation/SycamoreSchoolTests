f466411f6053d7a9d37a395616706a863c5788ea
Ray Wagner <raywagner88@me.com>
2018-06-07 14:39:18 -0500

    Add employeeDirectory object

---
+++ objects/frontDesk/employeeDirectory.js
@@ -0,0 +1,15 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        listTab: 'body > ul.nav.nav-tabs > li:nth-child(1) > a',
+        photosTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
+        // List Tab Elements
+        name: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(1)',
+        position: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(2)',
+        manager: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(3)',
+        tomName: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(1)',
+        tomNote: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(5) > font > a',
+        johnName: 'body > div.tab-content > table > tbody > tr:nth-child(3) > td:nth-child(1)',
+        johnNote: 'body > div.tab-content > table > tbody > tr:nth-child(3) > td:nth-child(5) > font > a'
+    }
+};
