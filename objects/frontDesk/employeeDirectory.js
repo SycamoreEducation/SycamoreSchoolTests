5bf1f73a0db72f4d524049c31e88f1ea9758a282
Ray Wagner <raywagner88@me.com>
2018-06-08 10:14:38 -0500

    Fix comments

--- objects/frontDesk/employeeDirectory.js
+++ objects/frontDesk/employeeDirectory.js
@@ -3,7 +3,7 @@ module.exports = {
         breadcrumb: 'body > ul.breadcrumb',
         listTab: 'body > ul.nav.nav-tabs > li:nth-child(1) > a',
         photosTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
-        // List Tab Elements
+        //List Tab Elements
         name: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(1)',
         position: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(2)',
         manager: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(3)',
@@ -11,7 +11,7 @@ module.exports = {
         tomNote: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(5) > font > a',
         johnName: 'body > div.tab-content > table > tbody > tr:nth-child(3) > td:nth-child(1)',
         johnNote: 'body > div.tab-content > table > tbody > tr:nth-child(3) > td:nth-child(5) > font > a',
-        // Photos Tab Elements
+        //Photos Tab Elements
         tomPhoto: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(1) > table > tbody > tr:nth-child(1) > td > a',
         tomCaption: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(1) > table > tbody > tr:nth-child(2) > td',
         johnPhoto: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr:nth-child(1) > td > a',
