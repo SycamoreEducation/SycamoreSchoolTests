ed215a211dbcf07d37086b467e924ff2ae3b4df6
Ray Wagner <raywagner88@me.com>
2018-06-11 14:39:53 -0500

    Add browse tab test

---
+++ tests/frontDeskTests/facilityTests/browse.js
@@ -0,0 +1,41 @@
+module.exports = {
+    tags: ['frontDeskTests', 'facilityTests', 'browseTabTest'],
+
+    'School News Archive Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@frontDesk')
+            .click('@frontDesk')
+            .assert.visible('@frontDeskFacility')
+            .click('@frontDeskFacility')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.frontDesk.facility()
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 'Front Desk/ Facility')
+
+            .assert.visible('@browseTab')
+            .assert.containsText('@browseTab', 'Browse')
+
+            .assert.visible('@plannerTab')
+            .assert.containsText('@plannerTab', 'Planner')
+
+            .assert.visible('@activitiesTab')
+            .assert.containsText('@activitiesTab', 'Activities')
+
+            .assert.visible('@searchTab')
+            .assert.containsText('@searchTab', 'Search')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
