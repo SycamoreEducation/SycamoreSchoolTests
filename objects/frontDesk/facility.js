d5e710dc6afd87bcb38a8880027321010c39b4ba
Ray Wagner <raywagner88@me.com>
2018-06-11 15:28:52 -0500

    Update page objects for activities tab

--- objects/frontDesk/facility.js
+++ objects/frontDesk/facility.js
@@ -5,12 +5,12 @@ module.exports = {
         plannerTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
         activitiesTab: 'body > ul.nav.nav-tabs > li:nth-child(3) > a',
         searchTab: 'body > ul.nav.nav-tabs > li:nth-child(4) > a',
-        dateInput: '#plannerdate',
         goBtn: 'body > form:nth-child(5) > table > tbody > tr > td > input.btn.btn-mini.btn-primary.pull-right',
         backDayBtn: 'body > form:nth-child(6) > table > tbody > tr > td:nth-child(1) > a',
         forwardDayBtn: 'body > form:nth-child(6) > table > tbody > tr > td.se-right > a',
         dayHeader: 'body > form:nth-child(6) > table > tbody > tr > td.se-font-title.se-nowrap.se-bold',
         //Planner Tab
+        plannerDateInput: '#plannerdate',
         sixAm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(3)',
         sevenAm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(4)',
         eightAm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(5)',
@@ -26,6 +26,8 @@ module.exports = {
         sixPm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(15)',
         sevenPm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(16)',
         eightPm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(17)',
-        ninePm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(18)'
+        ninePm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(18)',
+        //ActiviesTab
+        activitiesDateInput: '#actdate'
     }
 };
