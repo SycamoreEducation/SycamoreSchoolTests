db01be0c48997941264a7a8ebae4fe03c5a57b65
Ray Wagner <raywagner88@me.com>
2018-06-11 15:20:16 -0500

    Update page objects for planner tab

--- objects/frontDesk/facility.js
+++ objects/frontDesk/facility.js
@@ -4,6 +4,28 @@ module.exports = {
         browseTab: 'body > ul.nav.nav-tabs > li:nth-child(1) > a',
         plannerTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
         activitiesTab: 'body > ul.nav.nav-tabs > li:nth-child(3) > a',
-        searchTab: 'body > ul.nav.nav-tabs > li:nth-child(4) > a'
+        searchTab: 'body > ul.nav.nav-tabs > li:nth-child(4) > a',
+        dateInput: '#plannerdate',
+        goBtn: 'body > form:nth-child(5) > table > tbody > tr > td > input.btn.btn-mini.btn-primary.pull-right',
+        backDayBtn: 'body > form:nth-child(6) > table > tbody > tr > td:nth-child(1) > a',
+        forwardDayBtn: 'body > form:nth-child(6) > table > tbody > tr > td.se-right > a',
+        dayHeader: 'body > form:nth-child(6) > table > tbody > tr > td.se-font-title.se-nowrap.se-bold',
+        //Planner Tab
+        sixAm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(3)',
+        sevenAm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(4)',
+        eightAm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(5)',
+        nineAm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(6)',
+        tenAm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(7)',
+        elevenAm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(8)',
+        twelvePm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(9)',
+        onePm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(10)',
+        twoPm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(11)',
+        threePm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(12)',
+        fourPm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(13)',
+        fivePm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(14)',
+        sixPm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(15)',
+        sevenPm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(16)',
+        eightPm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(17)',
+        ninePm: 'body > form:nth-child(6) > div > table > tbody > tr.se-bg-gray > td:nth-child(18)'
     }
 };
