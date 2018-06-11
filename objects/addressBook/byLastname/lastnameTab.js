242e21c32973f2e8a89ac8b1ba9d7e8e82bc1d02
Ray Wagner <raywagner88@me.com>
2018-06-11 10:23:50 -0500

    Update object

--- objects/addressBook/byLastname/lastnameTab.js
+++ objects/addressBook/byLastname/lastnameTab.js
@@ -37,15 +37,10 @@ module.exports = {
         x: 'body > form > table > tbody > tr > td > table > tbody > tr > td:nth-child(25) > a',
         y: 'body > form > table > tbody > tr > td > table > tbody > tr > td:nth-child(26) > a',
         z: 'body > form > table > tbody > tr > td > table > tbody > tr > td:nth-child(27) > a',
-        janeName: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > font > a',
-        janeEnvelope: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(2) > td:nth-child(6) > a',
-        janeNote: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(2) > td:nth-child(7) > a:nth-child(1)',
-        janeDocs: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(2) > td:nth-child(7) > a:nth-child(2)',
-        janeCard: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(2) > td:nth-child(7) > a:nth-child(3)',
-        johnName: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(3) > td:nth-child(2) > font > a',
-        johnEnvelope: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(3) > td:nth-child(6) > a',
-        johnNote: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(3) > td:nth-child(7) > a:nth-child(1)',
-        johnDocs: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(3) > td:nth-child(7) > a:nth-child(2)',
-        johnCard: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(3) > td:nth-child(7) > a:nth-child(3)'
+        name: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > font > a',
+        envelope: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(2) > td:nth-child(6) > a',
+        note: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(2) > td:nth-child(7) > a:nth-child(1)',
+        docs: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(2) > td:nth-child(7) > a:nth-child(2)',
+        card: 'body > form > table > tbody > tr > td > div > table > tbody > tr:nth-child(2) > td:nth-child(7) > a:nth-child(3)'
     }
 };
