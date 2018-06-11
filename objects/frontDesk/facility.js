3a6d9fd9ef7c630e99f912b56626ea5576d596b2
Ray Wagner <raywagner88@me.com>
2018-06-11 14:39:39 -0500

    Add facility page objects

---
+++ objects/frontDesk/facility.js
@@ -0,0 +1,9 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        browseTab: 'body > ul.nav.nav-tabs > li:nth-child(1) > a',
+        plannerTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
+        activitiesTab: 'body > ul.nav.nav-tabs > li:nth-child(3) > a',
+        searchTab: 'body > ul.nav.nav-tabs > li:nth-child(4) > a'
+    }
+};
