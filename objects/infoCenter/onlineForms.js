6c94e453acb05585b495968382b3bc95e38a97cc
Ray Wagner <raywagner88@me.com>
2018-06-12 15:57:30 -0500

    Add available tab page objects

---
+++ objects/infoCenter/onlineForms.js
@@ -0,0 +1,10 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul',
+        availableTab: 'body > form > ul > li:nth-child(1) > a',
+        submittedTab: 'body > form > ul > li:nth-child(2) > a',
+        //Available Tab Elements
+        availableNameHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(2)',
+        availableDescriptionHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(3)',
+    }
+};
