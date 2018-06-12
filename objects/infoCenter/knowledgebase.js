8ab8538062b981123bbfb1b643845345079e6afb
Ray Wagner <raywagner88@me.com>
2018-06-12 16:32:45 -0500

    Add browse tab page objects

---
+++ objects/infoCenter/knowledgebase.js
@@ -0,0 +1,9 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        browseTab: 'body > ul.nav.nav-tabs > li:nth-child(1) > a',
+        searchTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
+        //Browse Tab Elements
+        newQuestionBtn: 'body > ul.nav.nav-tabs > input'
+    }
+};
