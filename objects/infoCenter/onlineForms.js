3b55a83912a0dffd0c84ec1666bcb945858c8155
Ray Wagner <raywagner88@me.com>
2018-06-12 16:03:06 -0500

    Add submitted tab page objects

--- objects/infoCenter/onlineForms.js
+++ objects/infoCenter/onlineForms.js
@@ -6,5 +6,9 @@ module.exports = {
         //Available Tab Elements
         availableNameHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(2)',
         availableDescriptionHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(3)',
+        //Submitted Tab Elements
+        submittedHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(2)',
+        submittedNameHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(3)',
+        submittedDescriptionHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(4)'
     }
 };
