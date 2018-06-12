ff9cef95d00a152593e866e78d25a79cc80c3770
Ray Wagner <raywagner88@me.com>
2018-06-12 16:37:22 -0500

    Add search tab page objects

--- objects/infoCenter/knowledgebase.js
+++ objects/infoCenter/knowledgebase.js
@@ -4,6 +4,10 @@ module.exports = {
         browseTab: 'body > ul.nav.nav-tabs > li:nth-child(1) > a',
         searchTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
         //Browse Tab Elements
-        newQuestionBtn: 'body > ul.nav.nav-tabs > input'
+        newQuestionBtn: 'body > ul.nav.nav-tabs > input',
+        //Serach Tab Elements
+        searchInput: 'body > form > div > table > tbody > tr:nth-child(1) > td > input[type="text"]:nth-child(1)',
+        searchBtn: 'body > form > div > table > tbody > tr:nth-child(1) > td > input.btn.btn-mini.btn-primary',
+        questionHeader: 'body > form > div > table > tbody > tr:nth-child(2) > td'
     }
 };
