12589efdf3117530bf07c50e8b2895e465772893
Ray Wagner <raywagner88@me.com>
2018-06-12 16:32:54 -0500

    Add browse tab test

---
+++ tests/infoCenterTests/knowledgebaseTests/browseTest.js
@@ -0,0 +1,41 @@
+module.exports = {
+    tags: ['infoCenterTests', 'infoCenterKnowledgebaseTests',
+        'knowledgebaseBrowseTest'],
+
+    'Info Center Knowledgebase Browse Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@infoCenter')
+            .click('@infoCenter')
+            .assert.visible('@infoCenterKnowledgebase')
+            .click('@infoCenterKnowledgebase')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.infoCenter.knowledgebase()
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 'Knowledge Base')
+
+            .assert.visible('@browseTab')
+            .assert.containsText('@browseTab', 'Browse')
+
+            .assert.visible('@searchTab')
+            .assert.containsText('@searchTab', 'Search')
+
+            .assert.visible('@newQuestionBtn')
+            .assert.attributeEquals('@newQuestionBtn', 'value', 'New Question')
+            .assert.attributeEquals('@newQuestionBtn', 'onclick', 
+                'newquestion()')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
