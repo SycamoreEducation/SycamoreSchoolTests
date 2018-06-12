0f94804d6fbcbb7f788a4de831ec6f6392e4979f
Ray Wagner <raywagner88@me.com>
2018-06-12 15:57:44 -0500

    Add available tab test

---
+++ tests/infoCenterTests/onlineFormsTests/availableTest.js
@@ -0,0 +1,44 @@
+var moment = require('moment');
+
+module.exports = {
+    tags: ['infoCenterTests', 'infoCenterOnlineFormsTests', 
+        'onlineFormsAvailableTest'],
+
+    'Info Center Online Forms Available Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@infoCenter')
+            .click('@infoCenter')
+            .assert.visible('@infoCenterOnlineForms')
+            .click('@infoCenterOnlineForms')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.infoCenter.onlineForms()
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 'Info Center/ Online Forms')
+
+            .assert.visible('@availableTab')
+            .assert.containsText('@availableTab', 'Available')
+
+            .assert.visible('@submittedTab')
+            .assert.containsText('@submittedTab', 'Submitted')
+
+            .assert.visible('@availableNameHeader')
+            .assert.containsText('@availableNameHeader', 'Name')
+
+            .assert.visible('@availableDescriptionHeader')
+            .assert.containsText('@availableDescriptionHeader', 'Description')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
