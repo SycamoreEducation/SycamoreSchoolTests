1d894f3b2910207813dd912687dec42c73e7e6ca
Ray Wagner <raywagner88@me.com>
2018-06-13 09:58:33 -0500

    Update naming convention

--- objects/menu.js
+++ objects/menu.js
@@ -547,13 +547,13 @@ module.exports = {
         myBatchEmail: '#menu > li:nth-child(24) > ul > li:nth-child(1) > a',
         myBlogs: '#menu > li:nth-child(24) > ul > li:nth-child(2) > a',
         //Pass-A-Note Menu
-        passANote: '#menu > li:nth-child(25) > a',
-        passANoteInbox: '#menu > li:nth-child(25) > ul > li:nth-child(1) > a',
-        passANoteOutbox: '#menu > li:nth-child(25) > ul > li:nth-child(2) > a',
-        passANoteEmployees: '#menu > li:nth-child(25) > ul > li:nth-child(3) > a',
-        passANoteAssociates: '#menu > li:nth-child(25) > ul > li:nth-child(4) > a',
-        passANoteFamilies: '#menu > li:nth-child(25) > ul > li:nth-child(5) > a',
-        passANoteStudents: '#menu > li:nth-child(25) > ul > li:nth-child(6) > a',
-        passANoteOptions: '#menu > li:nth-child(25) > ul > li:nth-child(7) > a'
+        PAN: '#menu > li:nth-child(25) > a',
+        PANInbox: '#menu > li:nth-child(25) > ul > li:nth-child(1) > a',
+        PANOutbox: '#menu > li:nth-child(25) > ul > li:nth-child(2) > a',
+        PANEmployees: '#menu > li:nth-child(25) > ul > li:nth-child(3) > a',
+        PANAssociates: '#menu > li:nth-child(25) > ul > li:nth-child(4) > a',
+        PANFamilies: '#menu > li:nth-child(25) > ul > li:nth-child(5) > a',
+        PANStudents: '#menu > li:nth-child(25) > ul > li:nth-child(6) > a',
+        PANOptions: '#menu > li:nth-child(25) > ul > li:nth-child(7) > a'
     }
 };
