29ffc8598656160cba2846323d89249d5416ec3e
Ray Wagner <raywagner88@me.com>
2018-06-14 16:07:23 -0500

    Add common tab page objects

---
+++ objects/addressBook/reports/reports.js
@@ -0,0 +1,26 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        commonTab: 'body > ul.nav.nav-tabs > li:nth-child(1) > a',
+        customTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
+        nameHeader: 'body > div.tab-content > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(1)',
+        descriptionHeader: 'body > div.tab-content > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(2)',
+        //Common Reports
+        companyLabels: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(1) > a',
+        companyLabelsDescription: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(2)',
+        contactAdditionalFieldEntry: 'body > div.tab-content > table > tbody > tr:nth-child(3) > td:nth-child(1) > a',
+        contactAdditionalFieldEntryDescription: 'body > div.tab-content > table > tbody > tr:nth-child(3) > td:nth-child(2)',
+        contactCategoryCounter: 'body > div.tab-content > table > tbody > tr:nth-child(4) > td:nth-child(1) > a',
+        contactCategoryCounterDescription: 'body > div.tab-content > table > tbody > tr:nth-child(4) > td:nth-child(2)',
+        contactEnvelopes: 'body > div.tab-content > table > tbody > tr:nth-child(5) > td:nth-child(1) > a',
+        contactEnvelopesDescription: 'body > div.tab-content > table > tbody > tr:nth-child(5) > td:nth-child(2)',
+        contactLabels: 'body > div.tab-content > table > tbody > tr:nth-child(6) > td:nth-child(1) > a',
+        contactLabelsDescription: 'body > div.tab-content > table > tbody > tr:nth-child(6) > td:nth-child(2)',
+        duplicateRecords: 'body > div.tab-content > table > tbody > tr:nth-child(7) > td:nth-child(1) > a',
+        duplicateRecordsDescription: 'body > div.tab-content > table > tbody > tr:nth-child(7) > td:nth-child(2)',
+        dynamicCompany: 'body > div.tab-content > table > tbody > tr:nth-child(8) > td:nth-child(1) > a',
+        dynamicCompanyDescription: 'body > div.tab-content > table > tbody > tr:nth-child(8) > td:nth-child(2)',
+        dynamicContact: 'body > div.tab-content > table > tbody > tr:nth-child(9) > td:nth-child(1) > a',
+        dynamicContactDescription: 'body > div.tab-content > table > tbody > tr:nth-child(9) > td:nth-child(2)'
+    }
+};
