ee9e3fbe163dd1ea0a5240a1489178777b4ef612
Ray Wagner <raywagner88@me.com>
2018-06-14 16:09:31 -0500

    Add custom reports tab

---
+++ tests/addressBookTests/reportsTests/customTest.js
@@ -0,0 +1,45 @@
+module.exports = {
+    tags: ['addressBookTests', 'addressBookReportsTests', 
+        'reportsCustomTabTest'],
+
+    'Address Book Reports Custom Tab Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@addressBook')
+            .click('@addressBook')
+            .assert.visible('@addressBookReports')
+            .click('@addressBookReports')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.addressBook.reports.reports()
+            .waitForElementVisible('@customTab', 5000)
+            .click('@customTab')
+
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 'Address Book/ Reports')
+            
+            .assert.visible('@commonTab')
+            .assert.containsText('@commonTab', 'Common')
+
+            .assert.visible('@customTab')
+            .assert.containsText('@customTab', 'Custom')
+
+            .assert.visible('@nameHeader')
+            .assert.containsText('@nameHeader', 'Name')
+
+            .assert.visible('@descriptionHeader')
+            .assert.containsText('@descriptionHeader', 'Description')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
