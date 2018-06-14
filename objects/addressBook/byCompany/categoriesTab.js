0433cf72e630c281591f693525b3a5f243e6bbf4
Ray Wagner <raywagner88@me.com>
2018-06-14 14:27:45 -0500

    Add categories page object

---
+++ objects/addressBook/byCompany/categoriesTab.js
@@ -0,0 +1,15 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > form > ul.breadcrumb',
+        companiesTab: 'body > form > ul.nav.nav-tabs > li:nth-child(1) > a',
+        categoriesTab: 'body > form > ul.nav.nav-tabs > li:nth-child(2) > a',
+        searchTab: 'body > form > ul.nav.nav-tabs > li:nth-child(3) > a',
+        searchInput: 'body > form > ul.nav.nav-tabs > input[type="input"]:nth-child(4)',
+        searchBtn: 'body > form > ul.nav.nav-tabs > input[type="submit"]:nth-child(5)',
+        newCompanyBtn: 'body > form > ul.nav.nav-tabs > input[type="button"]:nth-child(7)',
+        newContactBtn: 'body > form > ul.nav.nav-tabs > input[type="button"]:nth-child(6)',
+        categoryLabel: 'body > form > table:nth-child(4) > tbody > tr > td:nth-child(1) > font',
+        categorySelect: 'body > form > table:nth-child(4) > tbody > tr > td:nth-child(1) > b > select',
+        companyNameHeader: 'body > form > table:nth-child(5) > tbody > tr:nth-child(1) > td:nth-child(2)'
+    }
+};
