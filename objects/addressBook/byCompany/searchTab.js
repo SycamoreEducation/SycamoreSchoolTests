d79eec65190d29820d1a57f906a3d58a62115b6a
Ray Wagner <raywagner88@me.com>
2018-06-14 15:02:11 -0500

    Add search tab page objects

---
+++ objects/addressBook/byCompany/searchTab.js
@@ -0,0 +1,26 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > form > ul:nth-child(2)',
+        companiesTab: 'body > form > ul.nav.nav-tabs > li:nth-child(1) > a',
+        categoriesTab: 'body > form > ul.nav.nav-tabs > li:nth-child(2) > a',
+        searchTab: 'body > form > ul.nav.nav-tabs > li:nth-child(3) > a',
+        searchBtn: 'body > form > ul.nav.nav-tabs > input',
+        companyLabel: 'body > form > table > tbody > tr:nth-child(2) > td:nth-child(1)',
+        companyInput: 'body > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > input',
+        address1Label: 'body > form > table > tbody > tr:nth-child(3) > td:nth-child(1)',
+        address1Input: 'body > form > table > tbody > tr:nth-child(3) > td:nth-child(2) > input',
+        address2Label: 'body > form > table > tbody > tr:nth-child(4) > td:nth-child(1)',
+        address2Input: 'body > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > input',
+        cityLabel: 'body > form > table > tbody > tr:nth-child(5) > td:nth-child(1)',
+        cityInput: 'body > form > table > tbody > tr:nth-child(5) > td:nth-child(2) > input',
+        stateLabel: 'body > form > table > tbody > tr:nth-child(6) > td:nth-child(1)',
+        stateSelect: 'body > form > table > tbody > tr:nth-child(6) > td:nth-child(2) > select',
+        zipLabel: 'body > form > table > tbody > tr:nth-child(7) > td:nth-child(1)',
+        zipInput: 'body > form > table > tbody > tr:nth-child(7) > td:nth-child(2) > input',
+        phoneLabel: 'body > form > table > tbody > tr:nth-child(8) > td:nth-child(1)',
+        phoneInput: 'body > form > table > tbody > tr:nth-child(8) > td:nth-child(2) > input',
+        emailLabel: 'body > form > table > tbody > tr:nth-child(9) > td:nth-child(1)',
+        emailInput: 'body > form > table > tbody > tr:nth-child(9) > td:nth-child(2) > input',
+        resultsHeader: 'body > form > ul:nth-child(5) > li > a'
+    }
+};
