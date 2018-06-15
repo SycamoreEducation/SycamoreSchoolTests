9472d759d4d3047f0e1b79df8ed62949ab084083
Ray Wagner <raywagner88@me.com>
2018-06-15 10:22:50 -0500

    Add sponsor ads page object

---
+++ objects/communication/homepage/sponsorAds.js
@@ -0,0 +1,11 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > form > ul.breadcrumb',
+        currentTab: 'body > form > ul.nav.nav-tabs > li > a',
+        newAdBtn: 'body > form > ul.nav.nav-tabs > input',
+        headlineHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(2)',
+        startHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(3)',
+        stopHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(4)',
+        authorHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(5)'
+    }
+};
