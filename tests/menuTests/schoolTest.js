8bd5d4794c74db6bd90da56125cad46d398a3a2f
Ray Wagner <raywagner88@me.com>
2018-06-15 09:56:47 -0500

    Update school assertion order

--- tests/menuTests/schoolTest.js
+++ tests/menuTests/schoolTest.js
@@ -12,7 +12,8 @@ module.exports = {
         client.page.menu()
             .assert.visible('@school')
             .assert.containsText('@school', '\273 School')
-            .assert.attributeEquals('@school', 'href', page.url() + '/schoolmenu.php#')
+            .assert.attributeEquals('@school', 'href', 
+                page.url() + '/schoolmenu.php#')
 
             .assert.hidden('@schoolNews')
             .assert.hidden('@schoolCalendar')
@@ -22,26 +23,39 @@ module.exports = {
             .assert.hidden('@schoolFavorites')
 
             .click('@school')
+
             .assert.visible('@schoolNews')
-            .assert.visible('@schoolCalendar')
-            .assert.visible('@schoolDocuments')
-            .assert.visible('@schoolPhotoAlbums')
-            .assert.visible('@schoolMultimedia')
-            .assert.visible('@schoolFavorites')
             .assert.containsText('@schoolNews', 'News')
+            .assert.attributeEquals('@schoolNews', 'href', 
+                page.url() + '/news.php?classid=0')
+
+            .assert.visible('@schoolCalendar')
             .assert.containsText('@schoolCalendar', 'Calendar')
+            .assert.attributeEquals('@schoolCalendar', 'href', 
+                page.url() + '/calendar.php?classid=0')
+
+            .assert.visible('@schoolDocuments')
             .assert.containsText('@schoolDocuments', 'Documents')
+            .assert.attributeEquals('@schoolDocuments', 'href', 
+                page.url() + '/explorer.php?classid=0')
+
+            .assert.visible('@schoolPhotoAlbums')
             .assert.containsText('@schoolPhotoAlbums', 'Photo Albums')
+            .assert.attributeEquals('@schoolPhotoAlbums', 'href', 
+                page.url() + '/photos.php?task=albums&classid=0')
+
+            .assert.visible('@schoolMultimedia')
             .assert.containsText('@schoolMultimedia', 'Multimedia')
+            .assert.attributeEquals('@schoolMultimedia', 'href', 
+                page.url() + '/multimedia.php?classid=0')
+
+            .assert.visible('@schoolFavorites')
             .assert.containsText('@schoolFavorites', 'Favorites')
-            .assert.attributeEquals('@schoolNews', 'href', page.url() + '/news.php?classid=0')
-            .assert.attributeEquals('@schoolCalendar', 'href', page.url() + '/calendar.php?classid=0')
-            .assert.attributeEquals('@schoolDocuments', 'href', page.url() + '/explorer.php?classid=0')
-            .assert.attributeEquals('@schoolPhotoAlbums', 'href', page.url() + '/photos.php?task=albums&classid=0')
-            .assert.attributeEquals('@schoolMultimedia', 'href', page.url() + '/multimedia.php?classid=0')
-            .assert.attributeEquals('@schoolFavorites', 'href', page.url() + '/favorites.php?classid=0')
+            .assert.attributeEquals('@schoolFavorites', 'href', 
+                page.url() + '/favorites.php?classid=0')
 
             .click('@school')
+            
             .assert.hidden('@schoolNews')
             .assert.hidden('@schoolCalendar')
             .assert.hidden('@schoolDocuments')
