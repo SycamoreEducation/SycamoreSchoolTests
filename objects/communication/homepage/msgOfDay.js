246cec6efdf7421b87d6413bd1418582b810f7f5
Ray Wagner <raywagner88@me.com>
2018-06-15 12:45:41 -0500

    Add msg of day page objects

---
+++ objects/communication/homepage/msgOfDay.js
@@ -0,0 +1,9 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > form > ul.breadcrumb',
+        generalTab: 'body > form > ul.nav.nav-tabs > li > a',
+        newMessageBtn: 'body > form > ul.nav.nav-tabs > input',
+        dateHeader: 'body > form > div > table > tbody > tr:nth-child(1) > td:nth-child(3)',
+        titleHeader: 'body > form > div > table > tbody > tr:nth-child(1) > td:nth-child(4)'
+    }
+};
