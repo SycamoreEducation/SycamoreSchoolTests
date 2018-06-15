e1ce8a366fc6c05b4c138971411db9c6e9189721
Ray Wagner <raywagner88@me.com>
2018-06-15 09:16:48 -0500

    Add flash news page object

---
+++ objects/communication/homepage/flashNews.js
@@ -0,0 +1,11 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        currentTab: 'body > form > ul > li > a',
+        newFlashBtn: 'body > form > ul > input',
+        headlineHeader: 'body > form > div > table > tbody > tr:nth-child(1) > td:nth-child(2)',
+        startHeader: 'body > form > div > table > tbody > tr:nth-child(1) > td:nth-child(3)',
+        stopHeader: 'body > form > div > table > tbody > tr:nth-child(1) > td:nth-child(4)',
+        authorHeader: 'body > form > div > table > tbody > tr:nth-child(1) > td:nth-child(5)'
+    }
+};
