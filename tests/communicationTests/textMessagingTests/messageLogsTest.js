cab54d75706c2ebf8fce4870c27415b5d97a3ebe
Ray Wagner <raywagner88@me.com>
2018-06-18 10:20:58 -0500

    Add message logs test

---
+++ tests/communicationTests/textMessagingTests/messageLogsTest.js
@@ -0,0 +1,102 @@
+var moment = require('moment');
+
+module.exports = {
+    tags: ['communicationTests', 'textMessagingTets', 'messageLogsTest'],
+
+    'Message Logs Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@communication')
+            .click('@communication')
+            .assert.visible('@communicationTextMessaging')
+            .click('@communicationTextMessaging')
+            .assert.visible('@communicationTextMessagingMessageLogs')
+            .click('@communicationTextMessagingMessageLogs')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.communication.textMessaging.messageLogs()
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 
+                'Communication/Text Messaging/ SMS Logs')
+
+            .assert.visible('@jan')
+            .assert.containsText('@jan', 'Jan')
+
+            .assert.visible('@feb')
+            .assert.containsText('@feb', 'Feb')
+
+            .assert.visible('@mar')
+            .assert.containsText('@mar', 'Mar')
+
+            .assert.visible('@apr')
+            .assert.containsText('@apr', 'Apr')
+
+            .assert.visible('@may')
+            .assert.containsText('@may', 'May')
+
+            .assert.visible('@jun')
+            .assert.containsText('@jun', 'Jun')
+            
+            .assert.visible('@jul')
+            .assert.containsText('@jul', 'Jul')
+
+            .assert.visible('@aug')
+            .assert.containsText('@aug', 'Aug')
+
+            .assert.visible('@sep')
+            .assert.containsText('@sep', 'Sep')
+
+            .assert.visible('@oct')
+            .assert.containsText('@oct', 'Oct')
+
+            .assert.visible('@nov')
+            .assert.containsText('@nov', 'Nov')
+
+            .assert.visible('@dec')
+            .assert.containsText('@dec', 'Dec')
+
+            .assert.visible('@messageTotal')
+            .assert.containsText('@messageTotal', 
+                'Message Count: 0  Cost: 0.00')
+
+            .assert.visible('@yearLabel')
+            .assert.containsText('@yearLabel', 'Year')
+
+            .assert.visible('@yearSelect')
+            .assert.containsText('@yearSelect', moment().format('YYYY'))
+            .assert.attributeEquals('@yearSelect', 'onchange', 'changeyear()')
+
+            .assert.visible('@dateHeader')
+            .assert.containsText('@dateHeader', 'Date')
+
+            .assert.visible('@typeHeader')
+            .assert.containsText('@typeHeader', 'Type')
+
+            .assert.visible('@fromHeader')
+            .assert.containsText('@fromHeader', 'From')
+
+            .assert.visible('@recipientNameHeader')
+            .assert.containsText('@recipientNameHeader', 'Recipient Name')
+
+            .assert.visible('@recipientNumberHeader')
+            .assert.containsText('@recipientNumberHeader', 'Recipient Number')
+
+            .assert.visible('@vendorHeader')
+            .assert.containsText('@vendorHeader', 'Vendor')
+
+            .assert.visible('@statusHeader')
+            .assert.containsText('@statusHeader', 'Status')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
