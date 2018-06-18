83dc2059d481c0835dd994813a772d86b2632773
Ray Wagner <raywagner88@me.com>
2018-06-18 11:18:47 -0500

    Add safety alert page objects

---
+++ objects/communication/safetyAlert.js
@@ -0,0 +1,13 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > form > ul.breadcrumb',
+        standbyTab: 'body > form > ul.nav.nav-tabs > li > a',
+        clearBtn: 'body > form > ul.nav.nav-tabs > input',
+        typeLabel: 'body > form > div > table:nth-child(1) > tbody > tr:nth-child(1)',
+        timeLabel: 'body > form > div > table:nth-child(1) > tbody > tr:nth-child(2)',
+        authorLabel: 'body > form > div > table:nth-child(1) > tbody > tr:nth-child(3)',
+        messageLabel: 'body > form > div > table:nth-child(1) > tbody > tr:nth-child(4)',
+        timeHeader: 'body > form > div > table.table.table-striped.table-hover > tbody > tr:nth-child(1) > td:nth-child(2)',
+        userHeader: 'body > form > div > table.table.table-striped.table-hover > tbody > tr:nth-child(1) > td:nth-child(3)'
+    }
+};
