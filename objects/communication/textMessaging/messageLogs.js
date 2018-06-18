ee50c4f739a2f019fd39663e0220a7019ff87ab2
Ray Wagner <raywagner88@me.com>
2018-06-18 10:20:43 -0500

    Add message logs page objects

---
+++ objects/communication/textMessaging/messageLogs.js
@@ -0,0 +1,27 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > form > ul.breadcrumb',
+        jan: 'body > form > ul.nav.nav-tabs > li:nth-child(1) > a',
+        feb: 'body > form > ul.nav.nav-tabs > li:nth-child(2) > a',
+        mar: 'body > form > ul.nav.nav-tabs > li:nth-child(3) > a',
+        apr: 'body > form > ul.nav.nav-tabs > li:nth-child(4) > a',
+        may: 'body > form > ul.nav.nav-tabs > li:nth-child(5) > a',
+        jun: 'body > form > ul.nav.nav-tabs > li:nth-child(6) > a',
+        jul: 'body > form > ul.nav.nav-tabs > li:nth-child(7) > a',
+        aug: 'body > form > ul.nav.nav-tabs > li:nth-child(8) > a',
+        sep: 'body > form > ul.nav.nav-tabs > li:nth-child(9) > a',
+        oct: 'body > form > ul.nav.nav-tabs > li:nth-child(10) > a',
+        nov: 'body > form > ul.nav.nav-tabs > li:nth-child(11) > a',
+        dec: 'body > form > ul.nav.nav-tabs > li:nth-child(12) > a',
+        messageTotal: '#total',
+        yearLabel: 'body > form > table > tbody > tr > td.se-font-small.se-bold.se-right',
+        yearSelect: '#year',
+        dateHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(1)',
+        typeHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(2)',
+        fromHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(3)',
+        recipientNameHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(4)',
+        recipientNumberHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(5)',
+        vendorHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(6)',
+        statusHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(7)'
+    }
+};
