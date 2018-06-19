82a23636b1033e1d7b4c48088f59614651e9bbb8
Ray Wagner <raywagner88@me.com>
2018-06-19 13:19:16 -0500

    Add human resources page object

---
+++ objects/administrative/administration/schoolYears.js
@@ -0,0 +1,13 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        generalTab: 'body > ul.nav.nav-tabs > li > a',
+        newSchoolYearBtn: 'body > ul.nav.nav-tabs > input[type="button"]:nth-child(4)',
+        setCurrentYearBtn: 'body > ul.nav.nav-tabs > input[type="button"]:nth-child(2)',
+        nameHeader: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(3)',
+        descriptionHeader: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(4)',
+        daysHeader: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(7)',
+        hoursHeader: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(8)',
+        dbIdHeader: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(9)'
+    }
+};
