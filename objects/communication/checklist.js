9e499bb6dc52baffe0076839393ddc7f412c074e
Ray Wagner <raywagner88@me.com>
2018-06-19 14:22:54 -0500

    Add checklist page object

---
+++ objects/communication/checklist.js
@@ -0,0 +1,10 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        currentTab: 'body > ul.nav.nav-tabs > li > a',
+        newChecklistBtn: 'body > ul.nav.nav-tabs > input',
+        titleHeader: 'body > div > table > tbody > tr:nth-child(1) > td:nth-child(2)',
+        authorHeader: 'body > div > table > tbody > tr:nth-child(1) > td:nth-child(3)',
+        countHeader: 'body > div > table > tbody > tr:nth-child(1) > td:nth-child(4)'
+    }
+};
