1a838eea0d40c03cc9b10b86c894b03d3b900fcf
Ray Wagner <raywagner88@me.com>
2018-06-20 12:43:13 -0500

    Add custom tab test

---
+++ tests/communicationTests/reportsTests/customTabTest.js
@@ -0,0 +1,45 @@
+module.exports = {
+    tags: ['communicationTests', 'communicationReportsTests',
+        'communicationReportsCustomTest'],
+
+    'Communication Reports Custom Tab Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@communication')
+            .click('@communication')
+            .assert.visible('@communicationReports')
+            .click('@communicationReports')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.communication.reports()
+            .waitForElementVisible('@customTab', 5000)
+            .click('@customTab')
+
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 'Communication/ Reports')
+
+            .assert.visible('@commonTab')
+            .assert.containsText('@commonTab', 'Common')
+
+            .assert.visible('@customTab')
+            .assert.containsText('@customTab', 'Custom')
+
+            .assert.visible('@nameHeader')
+            .assert.containsText('@nameHeader', 'Name')
+
+            .assert.visible('@descriptionHeader')
+            .assert.containsText('@descriptionHeader', 'Description')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
