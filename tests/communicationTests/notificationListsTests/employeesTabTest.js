c98b331318757e0d7e28956840cc084be7225ba1
Ray Wagner <raywagner88@me.com>
2018-06-20 12:16:27 -0500

    Add employees tab test

---
+++ tests/communicationTests/notificationListsTests/employeesTabTest.js
@@ -0,0 +1,56 @@
+module.exports = {
+    tags: ['communicationTests', 'notificationListsTests', 
+        'notificationListsemployeesTabTest'],
+
+    'Notification Lists Employees Tab Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@communication')
+            .click('@communication')
+            .assert.visible('@communicationNotificationLists')
+            .click('@communicationNotificationLists')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.communication.notificationList()
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 
+                'Communication/ Notification Lists')
+
+            .assert.visible('@employeesTab')
+            .assert.containsText('@employeesTab', 'Employees')
+
+            .assert.visible('@parentsTab')
+            .assert.containsText('@parentsTab', 'Parents')
+
+            .assert.visible('@studentsTab')
+            .assert.containsText('@studentsTab', 'Students')
+
+            .assert.visible('@newListBtn')
+            .assert.attributeEquals('@newListBtn', 'value', ' New List ')
+            .assert.attributeEquals('@newListBtn', 'onclick', 'addlist(1)')
+            
+            .assert.visible('@nameHeader')
+            .assert.containsText('@nameHeader', 'Name')
+
+            .assert.visible('@descriptionHeader')
+            .assert.containsText('@descriptionHeader', 'Description')
+
+            .assert.visible('@optionsHeader')
+            .assert.containsText('@optionsHeader', 'Options')
+
+            .assert.visible('@countHeader')
+            .assert.containsText('@countHeader', 'Count')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
