47fcd89781e72ada6545e4fba12c5f926b80ffb3
Ray Wagner <raywagner88@me.com>
2018-06-20 12:41:12 -0500

    Add reports page object

---
+++ objects/communication/reports.js
@@ -0,0 +1,11 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        commonTab: 'body > ul.nav.nav-tabs > li:nth-child(1) > a',
+        customTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
+        nameHeader: 'body > div.tab-content > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(1)',
+        descriptionHeader: 'body > div.tab-content > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(2)',
+        testReportName: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(1) > a',
+        testReportDescription: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(2)' 
+    }
+};
