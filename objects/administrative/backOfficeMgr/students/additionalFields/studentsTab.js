7804770dc9c10de2ee05ff5346468207f4e35356
Ray Wagner <raywagner88@me.com>
2018-06-20 07:45:17 -0500

    Add students tab page object

---
+++ objects/administrative/backOfficeMgr/students/additionalFields/studentsTab.js
@@ -0,0 +1,15 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        studentsTab: 'body > ul.nav.nav-tabs > li > a',
+        newFieldBtn: 'body > ul.nav.nav-tabs > button:nth-child(3)',
+        groupsBtn: 'body > ul.nav.nav-tabs > button:nth-child(2)',
+        numericHeader: 'body > div.tab-content > table > tbody > tr.se-bold.se-bg-gray > td:nth-child(1)',
+        attributesHeader: 'body > div.tab-content > table > tbody > tr.se-bold.se-bg-gray > td:nth-child(2)',
+        typeHeader: 'body > div.tab-content > table > tbody > tr.se-bold.se-bg-gray > td:nth-child(3)',
+        descriptionHeader: 'body > div.tab-content > table > tbody > tr.se-bold.se-bg-gray > td:nth-child(4)',
+        profileHeader: 'body > div.tab-content > table > tbody > tr.se-bold.se-bg-gray > td:nth-child(5)',
+        addmissionsHeader: 'body > div.tab-content > table > tbody > tr.se-bold.se-bg-gray > td:nth-child(6)',
+        rpIdHeader: 'body > div.tab-content > table > tbody > tr.se-bold.se-bg-gray > td:nth-child(7)'
+    }
+};
