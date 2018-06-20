572908b403ecc02e36e63b760cdab8f36b98b7d7
Ray Wagner <raywagner88@me.com>
2018-06-20 12:16:15 -0500

    Add notification list page object

---
+++ objects/communication/notificationList.js
@@ -0,0 +1,13 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        employeesTab: 'body > ul.nav.nav-tabs > li:nth-child(1) > a',
+        parentsTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
+        studentsTab: 'body > ul.nav.nav-tabs > li:nth-child(3) > a',
+        newListBtn: 'body > ul.nav.nav-tabs > input',
+        nameHeader: 'body > div.tab-content > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(2)',
+        descriptionHeader: 'body > div.tab-content > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(3)',
+        optionsHeader: 'body > div.tab-content > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(4)',
+        countHeader: 'body > div.tab-content > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(5)'
+    }
+};
