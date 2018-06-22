2f3d76d7c8e8069ebbee8696131e572f812ac899
Ray Wagner <raywagner88@me.com>
2018-06-22 09:26:45 -0500

    Add items tab test

---
+++ tests/administrativeTests/backOfficeMgrTests/studentTests/checklistTests/itemsTabTest.js
@@ -0,0 +1,54 @@
+module.exports = {
+    tags: ['administrativeTests', 'backOfficeMgrTests', 'backOfficeStudentTests',
+        'backOfficeStudentChecklistTests', 
+        'backOfficeStudentChecklistItemsTabTest'],
+
+    'Back Office Student Types Current Tab Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@administrative')
+            .click('@administrative')
+            .assert.visible('@backOfficeMgr')
+            .click('@backOfficeMgr')
+            .assert.visible('@backOfficeMgrStudents')
+            .click('@backOfficeMgrStudents')
+            .assert.visible('@backOfficeMgrStudentsChecklists')
+            .click('@backOfficeMgrStudentsChecklists')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.administrative.backOfficeMgr.students.checklist.itemsTab()
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 
+                'Administrative/Back Office/Students/ Checklists')
+
+            .activeTab('Items')
+
+            .assert.visible('@newItemBtn')
+            .assert.attributeEquals('@newItemBtn', 'value', 'New Item')
+            .assert.attributeEquals('@newItemBtn', 'onclick', 'additem()')
+
+            .assert.visible('@clearAllChecksBtn')
+            .assert.attributeEquals('@clearAllChecksBtn', 'value', 
+                'Clear All Checks')
+            .assert.attributeEquals('@clearAllChecksBtn', 'onclick', 
+                'clearall()')
+
+            .assert.visible('@itemHeader')
+            .assert.containsText('@itemHeader', 'Item')
+
+            .assert.visible('@descriptionHeader')
+            .assert.containsText('@descriptionHeader', 'Description')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
