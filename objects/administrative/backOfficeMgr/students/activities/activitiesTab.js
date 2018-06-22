ab706223978fab6945e5c636e324b8031935f148
Ray Wagner <raywagner88@me.com>
2018-06-22 08:47:22 -0500

    Add studnets activities tab page object

---
+++ objects/administrative/backOfficeMgr/students/activities/activitiesTab.js
@@ -0,0 +1,12 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        newActivityBtn: 'body > ul.nav.nav-tabs > input',
+        schoolYearLabel: 'body > table > tbody > tr > td > font',
+        schoolYearSelect: '#schoolyearid',
+        numericHeader: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(1)',
+        nameHeader: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(2)',
+        descriptionHeader: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(3)',
+        countHeader: 'body > div.tab-content > table > tbody > tr:nth-child(1) > td:nth-child(4)'
+    }
+};
