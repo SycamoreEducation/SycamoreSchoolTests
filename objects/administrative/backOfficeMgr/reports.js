a283e104a97df4d0dd9a3132e56627424f08466a
Ray Wagner <raywagner88@me.com>
2018-06-26 09:06:20 -0500

    Add common tab page objects

---
+++ objects/administrative/backOfficeMgr/reports.js
@@ -0,0 +1,13 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        commonTab: 'body > ul.nav.nav-tabs > li:nth-child(1) > a',
+        customTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
+        templatesTab: 'body > ul.nav.nav-tabs > li:nth-child(3) > a',
+        nameHeader: 'body > div.tab-content > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(1)',
+        descriptionHeader: 'body > div.tab-content > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(2)',
+        //Common Report
+        testReportName: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(1)',
+        testReportDescription: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(2)'
+    }
+};
