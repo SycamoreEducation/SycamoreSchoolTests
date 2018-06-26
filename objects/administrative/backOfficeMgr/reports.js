0e9f336658267c2d52f63769438b435d4d724367
Ray Wagner <raywagner88@me.com>
2018-06-26 10:44:24 -0500

    Update page object for templates tab

--- objects/administrative/backOfficeMgr/reports.js
+++ objects/administrative/backOfficeMgr/reports.js
@@ -1,13 +1,30 @@
 module.exports = {
     elements: {
-        breadcrumb: 'body > ul.breadcrumb',
-        commonTab: 'body > ul.nav.nav-tabs > li:nth-child(1) > a',
-        customTab: 'body > ul.nav.nav-tabs > li:nth-child(2) > a',
-        templatesTab: 'body > ul.nav.nav-tabs > li:nth-child(3) > a',
+        breadcrumb: 'ul.breadcrumb',
+        commonTab: 'ul.nav.nav-tabs > li:nth-child(1) > a',
+        customTab: 'ul.nav.nav-tabs > li:nth-child(2) > a',
+        templatesTab: 'ul.nav.nav-tabs > li:nth-child(3) > a',
         nameHeader: 'body > div.tab-content > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(1)',
         descriptionHeader: 'body > div.tab-content > table > tbody > tr.se-bg-gray.se-bold > td:nth-child(2)',
         //Common Report
         testReportName: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(1)',
-        testReportDescription: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(2)'
+        testReportDescription: 'body > div.tab-content > table > tbody > tr:nth-child(2) > td:nth-child(2)',
+        //Templates Tab
+        generateBtn: '#theButton',
+        studentTypeLabel: 'body > form > table > tbody > tr:nth-child(1) > td.se-right.se-bold',
+        studentTypeSelect: '#stypeid',
+        gradeLabel: 'body > form > table > tbody > tr:nth-child(2) > td.se-right.se-bold',
+        gradeSelect: '#grade',
+        gradeCaption: 'body > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > span',
+        reportTemplateLabel: 'body > form > table > tbody > tr:nth-child(3) > td.se-right.se-nowrap.se-bold',
+        reportTemplateSelect: '#rtid',
+        studentDocumentsLabel: 'body > form > table > tbody > tr:nth-child(3) > td:nth-child(3)',
+        studentDocumentsCheckbox: 'body > form > table > tbody > tr:nth-child(3) > td:nth-child(3) > input[type="checkbox"]',
+        checkmarkHeader: 'body > form > div > table > tbody > tr.se-bg-gray.se-font.se-bold > td.setChecked > a > img',
+        studentCodeLabel: 'body > form > div > table > tbody > tr.se-bg-gray.se-font.se-bold > td:nth-child(2)',
+        studentNameLabel: 'body > form > div > table > tbody > tr.se-bg-gray.se-font.se-bold > td:nth-child(3)',
+        testStudentCheckbox: 'body > form > div > table > tbody > tr:nth-child(2) > td.se-center > input',
+        testStudentCode: 'body > form > div > table > tbody > tr:nth-child(2) > td:nth-child(2)',
+        testStudetName: 'body > form > div > table > tbody > tr:nth-child(2) > td:nth-child(3)'.
     }
 };
