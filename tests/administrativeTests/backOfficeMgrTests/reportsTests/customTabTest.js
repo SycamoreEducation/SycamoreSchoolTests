115ae0170e8b243e67b9b94286dd2a19f31b4518
Ray Wagner <raywagner88@me.com>
2018-06-26 09:10:22 -0500

    Add custom tab test

---
+++ tests/administrativeTests/backOfficeMgrTests/reportsTests/customTabTest.js
@@ -0,0 +1,50 @@
+module.exports = {
+    tags: ['administrativeTests', 'backOfficeMgrTests', 'backOfficeReportsTests',
+        'backOfficeReportsCustomTabTest'],
+
+    'Back Office Reports Custom Tab Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@administrative')
+            .click('@administrative')
+            .assert.visible('@backOfficeMgr')
+            .click('@backOfficeMgr')
+            .assert.visible('@backOfficeReports')
+            .click('@backOfficeReports')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.administrative.backOfficeMgr.reports()
+            .waitForElementVisible('@customTab', 5000)
+            .click('@customTab')
+
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 
+                'Administrative/Back Office/ Reports')
+
+            .activeTab('Custom')
+
+            .assert.visible('@commonTab')
+            .assert.containsText('@commonTab', 'Common')
+
+            .assert.visible('@templatesTab')
+            .assert.containsText('@templatesTab', 'Templates')
+
+            .assert.visible('@nameHeader')
+            .assert.containsText('@nameHeader', 'Name')
+
+            .assert.visible('@descriptionHeader')
+            .assert.containsText('@descriptionHeader', 'Description')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
