eaaa36282d7cf0308c0271b01105c91e310a6ec5
Ray Wagner <raywagner88@me.com>
2018-06-26 09:06:44 -0500

    Add common tab test

---
+++ tests/administrativeTests/backOfficeMgrTests/reportsTests/commonTab.js
@@ -0,0 +1,51 @@
+module.exports = {
+    tags: ['administrativeTests', 'backOfficeMgrTests', 'backOfficeReportsTests',
+        'backOfficeReportsCommonTabTest'],
+
+    'Back Office Reports Common Tab Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@administrative')
+            .click('@administrative')
+            .assert.visible('@backOfficeMgr')
+            .click('@backOfficeMgr')
+            .assert.visible('@backOfficeReports')
+            .click('@backOfficeReports')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.administrative.backOfficeMgr.reports()
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 
+                'Administrative/Back Office/ Reports')
+
+            .activeTab('Common')
+
+            .assert.visible('@customTab')
+            .assert.containsText('@customTab', 'Custom')
+
+            .assert.visible('@templatesTab')
+            .assert.containsText('@templatesTab', 'Templates')
+
+            .assert.visible('@nameHeader')
+            .assert.containsText('@nameHeader', 'Name')
+
+            .assert.visible('@descriptionHeader')
+            .assert.containsText('@descriptionHeader', 'Description')
+
+            .assert.visible('@testReportName')
+
+            .assert.visible('@testReportDescription')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
