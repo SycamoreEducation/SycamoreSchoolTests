984b74c1420c936d65aa0981b7fe23d616be9542
Ray Wagner <raywagner88@me.com>
2018-06-28 15:38:09 -0500

    Add by classroom test

---
+++ tests/administrativeTests/attendanceTests/byClassroomTest.js
@@ -0,0 +1,116 @@
+var moment = require('moment');
+
+module.exports = {
+    tags: ['administrativeTests', 'attendanceTests', 'attendanceByClassTest'],
+
+    'Attendance Homeroom Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@administrative')
+            .click('@administrative')
+            .assert.visible('@attendance')
+            .click('@attendance')
+            .assert.visible('@attendanceByClassroom')
+            .click('@attendanceByClassroom')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.administrative.attendance.byClassroom()
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 
+                'Administrative/Attendance/ By Classrooms')
+
+            .activeTab('Attendance')
+
+            .assert.visible('@dateHeader')
+            .assert.containsText('@dateHeader', moment().format('MMMM YYYY'))
+
+            .assert.visible('@backMonthBtn')
+            .assert.attributeEquals('@backMonthBtn', 'href', page.url() + 
+                `/attendclasses.php?year=${moment().format('YYYY')}&` + 
+                `month=${moment().subtract(1, 'months').format('M')}`)
+
+            .assert.visible('@forwardMonthBtn')
+            .assert.attributeEquals('@forwardMonthBtn', 'href', page.url() + 
+                `/attendclasses.php?year=${moment().format('YYYY')}&` + 
+                `month=${moment().add(1, 'months').format('M')}`)
+
+            .assert.visible('@sun')
+            .assert.containsText('@sun', 'Sun')
+
+            .assert.visible('@mon')
+            .assert.containsText('@mon', 'Mon')
+
+            .assert.visible('@tue')
+            .assert.containsText('@tue', 'Tue')
+
+            .assert.visible('@wed')
+            .assert.containsText('@wed', 'Wed')
+
+            .assert.visible('@thu')
+            .assert.containsText('@thu', 'Thu')
+
+            .assert.visible('@fri')
+            .assert.containsText('@fri', 'Fri')
+
+            .assert.visible('@sat')
+            .assert.containsText('@sat', 'Sat')
+
+            .assert.visible('@typeLabel')
+            .assert.containsText('@typeLabel', 'Type')
+
+            .assert.visible('@typeSelect')
+            .assert.containsText('@typeSelect', 'Day Long')
+            .assert.attributeEquals('@typeSelect', 'onchange', 'newType()')
+
+            .assert.visible('@classHeader')
+            .assert.containsText('@classHeader', 'Class')
+
+            .assert.visible('@sectionHeader')
+            .assert.containsText('@sectionHeader', 'Section')
+
+            .assert.visible('@presentHeader')
+            .assert.containsText('@presentHeader', 'Present')
+
+            .assert.visible('@absentHeader')
+            .assert.containsText('@absentHeader', 'Absent')
+
+            .assert.visible('@tardyHeader')
+            .assert.containsText('@tardyHeader', 'Tardy')
+
+            .assert.visible('@totalHeader')
+            .assert.containsText('@totalHeader', 'Total')
+
+            .assert.visible('@attendanceTitle')
+            .assert.containsText('@attendanceTitle', 'Attendance')
+
+            .assert.visible('@attendanceName')
+            .assert.containsText('@attendanceName', 'Name')
+
+            .assert.visible('@attendancePresent')
+            .assert.containsText('@attendancePresent', 'Present')
+
+            .assert.visible('@attendanceTardy')
+            .assert.containsText('@attendanceTardy', 'Tardy')
+
+            .assert.visible('@attendanceExcused')
+            .assert.containsText('@attendanceExcused', 'Excused')
+
+            .assert.visible('@attendanceUnExcused')
+            .assert.containsText('@attendanceUnExcused', 'UnExcused')
+
+            .assert.visible('@attendanceNotes')
+            .assert.containsText('@attendanceNotes', 'Notes')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
