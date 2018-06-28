c79a524443c53e88b1d211451539626da536df58
Ray Wagner <raywagner88@me.com>
2018-06-28 09:06:00 -0500

    Update selector names

--- tests/administrativeTests/backOfficeMgrTests/familiesTests/newFamily.js/newTabTest.js
+++ tests/administrativeTests/backOfficeMgrTests/familiesTests/newFamily.js/newTabTest.js
@@ -48,193 +48,193 @@ module.exports = {
             .assert.visible('@parentsLabel')
             .assert.containsText('@parentsLabel', 'Parents')
 
-            .assert.visible('@title1Label')
-            .assert.containsText('@title1Label', 'Title')
+            .assert.visible('@titleLabel')
+            .assert.containsText('@titleLabel', 'Title')
 
-            .assert.visible('@title1Select')
+            .assert.visible('@titleSelect')
             
-            .assert.visible('@firstName1Label')
-            .assert.containsText('@firstName1Label', 'First Name')
+            .assert.visible('@firstNameLabel')
+            .assert.containsText('@firstNameLabel', 'First Name')
 
-            .assert.visible('@firstName1Input')
-            .assert.attributeEquals('@firstName1Input', 'type', 'text')
+            .assert.visible('@firstNameInput')
+            .assert.attributeEquals('@firstNameInput', 'type', 'text')
 
-            .assert.visible('@lastName1Label')
-            .assert.containsText('@lastName1Label', 'Last Name')
+            .assert.visible('@lastNameLabel')
+            .assert.containsText('@lastNameLabel', 'Last Name')
 
-            .assert.visible('@lastName1Input')
-            .assert.attributeEquals('@lastName1Input', 'type', 'text')
+            .assert.visible('@lastNameInput')
+            .assert.attributeEquals('@lastNameInput', 'type', 'text')
 
-            .assert.visible('@address1Label')
-            .assert.containsText('@address1Label', 'Address')
+            .assert.visible('@addressLabel')
+            .assert.containsText('@addressLabel', 'Address')
 
-            .assert.visible('@address1Input')
-            .assert.attributeEquals('@address1Input', 'type', 'text')
+            .assert.visible('@addressInput')
+            .assert.attributeEquals('@addressInput', 'type', 'text')
 
-            .assert.visible('@addressSecondary1Label')
-            .assert.containsText('@addressSecondary1Label', 'Address 2')
+            .assert.visible('@addressSecondaryLabel')
+            .assert.containsText('@addressSecondaryLabel', 'Address 2')
 
-            .assert.visible('@addressSecondary1Input')
-            .assert.attributeEquals('@addressSecondary1Input', 'type', 'text')
+            .assert.visible('@addressSecondaryInput')
+            .assert.attributeEquals('@addressSecondaryInput', 'type', 'text')
 
-            .assert.visible('@city1Label')
-            .assert.containsText('@city1Label', 'City')
+            .assert.visible('@cityLabel')
+            .assert.containsText('@cityLabel', 'City')
 
-            .assert.visible('@city1Input')
-            .assert.attributeEquals('@city1Input', 'type', 'text')
+            .assert.visible('@cityInput')
+            .assert.attributeEquals('@cityInput', 'type', 'text')
 
-            .assert.visible('@state1Label')
-            .assert.containsText('@state1Label', 'State/Region')
+            .assert.visible('@stateLabel')
+            .assert.containsText('@stateLabel', 'State/Region')
 
-            .assert.visible('@state1Input')
-            .assert.attributeEquals('@state1Input', 'type', 'text')
+            .assert.visible('@stateInput')
+            .assert.attributeEquals('@stateInput', 'type', 'text')
 
-            .assert.visible('@zip1Label')
-            .assert.containsText('@zip1Label', 'Zip/Postal Code')
+            .assert.visible('@zipLabel')
+            .assert.containsText('@zipLabel', 'Zip/Postal Code')
 
-            .assert.visible('@zip1Input')
-            .assert.attributeEquals('@zip1Input', 'type', 'text')
+            .assert.visible('@zipInput')
+            .assert.attributeEquals('@zipInput', 'type', 'text')
 
-            .assert.visible('@country1Label')
-            .assert.containsText('@country1Label', 'Country')
+            .assert.visible('@countryLabel')
+            .assert.containsText('@countryLabel', 'Country')
 
-            .assert.visible('@country1Input')
-            .assert.attributeEquals('@country1Input', 'type', 'text')
+            .assert.visible('@countryInput')
+            .assert.attributeEquals('@countryInput', 'type', 'text')
 
-            .assert.visible('@home1Label')
-            .assert.containsText('@home1Label', 'Home Phone')
+            .assert.visible('@homeLabel')
+            .assert.containsText('@homeLabel', 'Home Phone')
 
-            .assert.visible('@home1Input')
-            .assert.attributeEquals('@home1Input', 'type', 'text')
+            .assert.visible('@homeInput')
+            .assert.attributeEquals('@homeInput', 'type', 'text')
 
-            .assert.visible('@work1Label')
-            .assert.containsText('@work1Label', 'Work Phone')
+            .assert.visible('@workLabel')
+            .assert.containsText('@workLabel', 'Work Phone')
 
-            .assert.visible('@work1Input')
-            .assert.attributeEquals('@work1Input', 'type', 'text')
+            .assert.visible('@workInput')
+            .assert.attributeEquals('@workInput', 'type', 'text')
 
-            .assert.visible('@cell1Label')
-            .assert.containsText('@cell1Label', 'Cell Phone')
+            .assert.visible('@cellLabel')
+            .assert.containsText('@cellLabel', 'Cell Phone')
 
-            .assert.visible('@cell1Input')
-            .assert.attributeEquals('@cell1Input', 'type', 'text')
+            .assert.visible('@cellInput')
+            .assert.attributeEquals('@cellInput', 'type', 'text')
 
-            .assert.visible('@email1Label')
-            .assert.containsText('@email1Label', 'Email')
+            .assert.visible('@emailLabel')
+            .assert.containsText('@emailLabel', 'Email')
 
-            .assert.visible('@email1Input')
-            .assert.attributeEquals('@email1Input', 'type', 'text')
+            .assert.visible('@emailInput')
+            .assert.attributeEquals('@emailInput', 'type', 'text')
 
-            .assert.visible('@relationship1Label')
-            .assert.containsText('@relationship1Label', 'Relationship')
+            .assert.visible('@relationshipLabel')
+            .assert.containsText('@relationshipLabel', 'Relationship')
 
-            .assert.visible('@relationship1Select')
+            .assert.visible('@relationshipSelect')
 
-            .assert.visible('@company1Label')
-            .assert.containsText('@company1Label', 'Company')
+            .assert.visible('@companyLabel')
+            .assert.containsText('@companyLabel', 'Company')
 
-            .assert.visible('@company1Input')
-            .assert.attributeEquals('@company1Input', 'type', 'text')
+            .assert.visible('@companyInput')
+            .assert.attributeEquals('@companyInput', 'type', 'text')
 
-            .assert.visible('@companyTitle1Label')
-            .assert.containsText('@companyTitle1Label', 'Title')
+            .assert.visible('@companyTitleLabel')
+            .assert.containsText('@companyTitleLabel', 'Title')
             
-            .assert.visible('@companyTitle1Input')
-            .assert.attributeEquals('@companyTitle1Input', 'type', 'text')
+            .assert.visible('@companyTitleInput')
+            .assert.attributeEquals('@companyTitleInput', 'type', 'text')
 
-            .assert.visible('@title2Label')
-            .assert.containsText('@title2Label', 'Title')
+            .assert.visible('@titleLabel2')
+            .assert.containsText('@titleLabel2', 'Title')
 
-            .assert.visible('@title2Select')
+            .assert.visible('@titleSelect2')
             
-            .assert.visible('@firstName2Label')
-            .assert.containsText('@firstName2Label', 'First Name')
+            .assert.visible('@firstNameLabel2')
+            .assert.containsText('@firstNameLabel2', 'First Name')
 
-            .assert.visible('@firstName2Input')
-            .assert.attributeEquals('@firstName2Input', 'type', 'text')
+            .assert.visible('@firstNameInput2')
+            .assert.attributeEquals('@firstNameInput2', 'type', 'text')
 
-            .assert.visible('@lastName2Label')
-            .assert.containsText('@lastName2Label', 'Last Name')
+            .assert.visible('@lastNameLabel2')
+            .assert.containsText('@lastNameLabel2', 'Last Name')
 
-            .assert.visible('@lastName2Input')
-            .assert.attributeEquals('@lastName2Input', 'type', 'text')
+            .assert.visible('@lastNameInput2')
+            .assert.attributeEquals('@lastNameInput2', 'type', 'text')
 
-            .assert.visible('@address2Label')
-            .assert.containsText('@address2Label', 'Address')
+            .assert.visible('@addressLabel2')
+            .assert.containsText('@addressLabel2', 'Address')
 
-            .assert.visible('@address2Input')
-            .assert.attributeEquals('@address2Input', 'type', 'text')
+            .assert.visible('@addressInput2')
+            .assert.attributeEquals('@addressInput2', 'type', 'text')
 
-            .assert.visible('@addressSecondary2Label')
-            .assert.containsText('@addressSecondary2Label', 'Address 2')
+            .assert.visible('@addressSecondaryLabel2')
+            .assert.containsText('@addressSecondaryLabel2', 'Address ')
 
-            .assert.visible('@addressSecondary2Input')
-            .assert.attributeEquals('@addressSecondary2Input', 'type', 'text')
+            .assert.visible('@addressSecondaryInput2')
+            .assert.attributeEquals('@addressSecondaryInput2', 'type', 'text')
 
-            .assert.visible('@city2Label')
-            .assert.containsText('@city2Label', 'City')
+            .assert.visible('@cityLabel2')
+            .assert.containsText('@cityLabel2', 'City')
 
-            .assert.visible('@city2Input')
-            .assert.attributeEquals('@city2Input', 'type', 'text')
+            .assert.visible('@cityInput2')
+            .assert.attributeEquals('@cityInput2', 'type', 'text')
 
-            .assert.visible('@state2Label')
-            .assert.containsText('@state2Label', 'State/Region')
+            .assert.visible('@stateLabel2')
+            .assert.containsText('@stateLabel2', 'State/Region')
 
-            .assert.visible('@state2Input')
-            .assert.attributeEquals('@state2Input', 'type', 'text')
+            .assert.visible('@stateInput2')
+            .assert.attributeEquals('@stateInput2', 'type', 'text')
 
-            .assert.visible('@zip2Label')
-            .assert.containsText('@zip2Label', 'Zip/Postal Code')
+            .assert.visible('@zipLabel2')
+            .assert.containsText('@zipLabel2', 'Zip/Postal Code')
 
-            .assert.visible('@zip2Input')
-            .assert.attributeEquals('@zip2Input', 'type', 'text')
+            .assert.visible('@zipInput2')
+            .assert.attributeEquals('@zipInput2', 'type', 'text')
 
-            .assert.visible('@country2Label')
-            .assert.containsText('@country2Label', 'Country')
+            .assert.visible('@countryLabel2')
+            .assert.containsText('@countryLabel2', 'Country')
 
-            .assert.visible('@country2Input')
-            .assert.attributeEquals('@country2Input', 'type', 'text')
+            .assert.visible('@countryInput2')
+            .assert.attributeEquals('@countryInput2', 'type', 'text')
 
-            .assert.visible('@home2Label')
-            .assert.containsText('@home2Label', 'Home Phone')
+            .assert.visible('@homeLabel2')
+            .assert.containsText('@homeLabel2', 'Home Phone')
 
-            .assert.visible('@home2Input')
-            .assert.attributeEquals('@home2Input', 'type', 'text')
+            .assert.visible('@homeInput2')
+            .assert.attributeEquals('@homeInput2', 'type', 'text')
 
-            .assert.visible('@work2Label')
-            .assert.containsText('@work2Label', 'Work Phone')
+            .assert.visible('@workLabel2')
+            .assert.containsText('@workLabel2', 'Work Phone')
 
-            .assert.visible('@work2Input')
-            .assert.attributeEquals('@work2Input', 'type', 'text')
+            .assert.visible('@workInput2')
+            .assert.attributeEquals('@workInput2', 'type', 'text')
 
-            .assert.visible('@cell2Label')
-            .assert.containsText('@cell2Label', 'Cell Phone')
+            .assert.visible('@cellLabel2')
+            .assert.containsText('@cellLabel2', 'Cell Phone')
 
-            .assert.visible('@cell2Input')
-            .assert.attributeEquals('@cell2Input', 'type', 'text')
+            .assert.visible('@cellInput2')
+            .assert.attributeEquals('@cellInput2', 'type', 'text')
 
-            .assert.visible('@email2Label')
-            .assert.containsText('@email2Label', 'Email')
+            .assert.visible('@emailLabel2')
+            .assert.containsText('@emailLabel2', 'Email')
 
-            .assert.visible('@email2Input')
-            .assert.attributeEquals('@email2Input', 'type', 'text')
+            .assert.visible('@emailInput2')
+            .assert.attributeEquals('@emailInput2', 'type', 'text')
 
-            .assert.visible('@relationship2Label')
-            .assert.containsText('@relationship2Label', 'Relationship')
+            .assert.visible('@relationshipLabel2')
+            .assert.containsText('@relationshipLabel2', 'Relationship')
 
-            .assert.visible('@relationship2Select')
+            .assert.visible('@relationshipSelect2')
 
-            .assert.visible('@company2Label')
-            .assert.containsText('@company2Label', 'Company')
+            .assert.visible('@companyLabel2')
+            .assert.containsText('@companyLabel2', 'Company')
 
-            .assert.visible('@company2Input')
-            .assert.attributeEquals('@company2Input', 'type', 'text')
+            .assert.visible('@companyInput2')
+            .assert.attributeEquals('@companyInput2', 'type', 'text')
 
-            .assert.visible('@companyTitle2Label')
-            .assert.containsText('@companyTitle2Label', 'Title')
+            .assert.visible('@companyTitleLabel2')
+            .assert.containsText('@companyTitleLabel2', 'Title')
             
-            .assert.visible('@companyTitle2Input')
-            .assert.attributeEquals('@companyTitle2Input', 'type', 'text')
+            .assert.visible('@companyTitleInput2')
+            .assert.attributeEquals('@companyTitleInput2', 'type', 'text')
 
             .assert.visible('@studentsLabel')
             .assert.containsText('@studentsLabel', 'Students')
