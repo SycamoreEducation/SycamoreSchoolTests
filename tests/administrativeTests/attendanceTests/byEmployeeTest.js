31bd879d00c604a4205b92b45316d9b1e2bf2025
Ray Wagner <raywagner88@me.com>
2018-06-29 09:01:51 -0500

    Add by employee test

---
+++ tests/administrativeTests/attendanceTests/byEmployeeTest.js
@@ -0,0 +1,92 @@
+var moment = require('moment');
+
+module.exports = {
+    tags: ['administrativeTests', 'attendanceTests', 
+        'attendanceByEmployeeTest'],
+
+    'Attendance Employee Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@administrative')
+            .click('@administrative')
+            .assert.visible('@attendance')
+            .click('@attendance')
+            .assert.visible('@attendanceByEmployee')
+            .click('@attendanceByEmployee')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.administrative.attendance.byEmployee()
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 
+                'Administrative/Attendance/ By Employee')
+
+            .activeTab('Attendance')
+
+            .assert.visible('@submitAttendanceBtn')
+            .assert.attributeEquals('@submitAttendanceBtn', 'value', 
+                'Submit Attendance')
+            .assert.attributeEquals('@submitAttendanceBtn', 'onclick', 
+                'doSubmit()')
+
+            .assert.visible('@monthSelect')
+            .assert.containsText('@monthSelect', moment().format('MMM'))
+            .assert.attributeEquals('@monthSelect', 'onchange', 'redo()')
+
+            .assert.visible('@daySelect')
+            .assert.containsText('@daySelect', moment().format('DD'))
+            .assert.attributeEquals('@daySelect', 'onchange', 'redo()')
+
+            .assert.visible('@yearSelect')
+            .assert.containsText('@yearSelect', moment().format('YYYY'))
+            .assert.attributeEquals('@yearSelect', 'onchange', 'redo()')
+
+            .assert.visible('@calendarBtn')
+            .assert.attributeEquals('@calendarBtn', 'src', page.url() + 
+                '/images/calendar.jpg')
+        
+            .assert.visible('@uncheckAllBtn')
+            .assert.attributeEquals('@uncheckAllBtn', 'value', ' Uncheck All ')
+            .assert.attributeEquals('@uncheckAllBtn', 'onclick', 'unCheckAll()')
+
+            .assert.visible('@employeeHeader')
+            .assert.containsText('@employeeHeader', 'Employee')
+
+            .assert.visible('@presentHeader')
+            .assert.containsText('@presentHeader', 'Present')
+
+            .assert.visible('@typeHeader')
+            .assert.containsText('@typeHeader', 'Type')
+
+            .assert.visible('@hoursHeader')
+            .assert.containsText('@hoursHeader', 'Hours')
+
+            .assert.visible('@notesHeader')
+            .assert.containsText('@notesHeader', 'Notes')
+
+            .assert.visible('@testViewBtn')
+            .assert.attributeEquals('@testViewBtn', 'src', page.url() + 
+                '/images/history.gif')
+
+            .assert.visible('@testEmployeeName')
+
+            .assert.visible('@testCheckbox')
+
+            .assert.visible('@testType')
+
+            .assert.visible('@testHours')
+
+            .assert.visible('@testNotes')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
