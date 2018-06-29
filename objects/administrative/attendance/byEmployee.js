42058a03cb538004fb9cc8094a3dc0b39633e391
Ray Wagner <raywagner88@me.com>
2018-06-29 09:01:38 -0500

    Add by employee page object

---
+++ objects/administrative/attendance/byEmployee.js
@@ -0,0 +1,22 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        submitAttendanceBtn: 'input.btn:nth-child(2)',
+        monthSelect: '#month',
+        daySelect: '#day',
+        yearSelect: '#year',
+        calendarBtn: '#trigger',
+        uncheckAllBtn: 'input.btn:nth-child(1)',
+        employeeHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)',
+        presentHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)',
+        typeHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(4)',
+        hoursHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(5)',
+        notesHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(6)',
+        testViewBtn: '.table > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > img:nth-child(1)',
+        testEmployeeName: '.table > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)',
+        testCheckbox: '.table > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(3) > input:nth-child(2)',
+        testType: '.table > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(4) > select:nth-child(1)',
+        testHours: '.table > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(5) > input:nth-child(1)',
+        testNotes: '.table > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(6) > input:nth-child(1)'
+    }
+};
