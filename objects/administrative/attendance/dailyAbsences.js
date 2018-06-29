ef78d53e27ba6752ff29e4207f645e54fc7327ae
Ray Wagner <raywagner88@me.com>
2018-06-29 09:27:57 -0500

    Add daily absences page object

---
+++ objects/administrative/attendance/dailyAbsences.js
@@ -0,0 +1,13 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        printBtn: '.btn',
+        backDayBtn: 'body > form:nth-child(4) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > a:nth-child(1)',
+        dateTitle: 'body > form:nth-child(4) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)',
+        forwardDayBtn: 'body > form:nth-child(4) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3) > a:nth-child(1)',
+        dateSelect: '#absentdate',
+        studentNameHeader: '.table > tbody:nth-child(1) > tr:nth-child(1)'
+    }
+};
+//http://web/attendabsents.php/?day=30&month=06&year=2018
+//http://web/attendabsents.php?day=30&month=06&year=2018
