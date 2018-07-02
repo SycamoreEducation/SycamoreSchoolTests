a7597bed14a0243d00d89c5b698ed18f25ea78fc
Ray Wagner <raywagner88@me.com>
2018-07-02 14:12:30 -0500

    Add attendance scale page object

---
+++ objects/administrative/attendance/attendanceScale.js
@@ -0,0 +1,10 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        numericHeader: '.se-bg-gray > td:nth-child(1)',
+        upperHeader: '.se-bg-gray > td:nth-child(2)',
+        lowerHeader: '.se-bg-gray > td:nth-child(3)',
+        valueHeader: '.se-bg-gray > td:nth-child(4)',
+        labelHeader: '.se-bg-gray > td:nth-child(5)'
+    }
+};
