090d6754963396f3df111a4174ace61ef04a1be1
Ray Wagner <raywagner88@me.com>
2018-07-02 10:56:32 -0500

    Add guest tab page object

--- objects/administrative/attendance/checkInOut/guestTab.js
+++ objects/administrative/attendance/checkInOut/guestTab.js
@@ -1,22 +1,21 @@
 module.exports = {
     elements: {
         breadcrumb: '.breadcrumb',
-        studentsTab: '.nav > li:nth-child(1) > a',
-        guestTab: '.nav > li:nth-child(2) > a',
+        studentsTab: 'form > ul > li:nth-child(1) > a',
+        guestTab: 'form > ul > li:nth-child(2) > a',
         refreshBtn: 'input.btn:nth-child(3)',
         monthSelect: '#month',
         daySelect: '#day',
         yearSelect: '#year',
-        modeSelect: '#mode',
-        nameInput: '#fname',
-        reasonInput: '#reason',
+        guestNameInput: '#fname',
+        toSeeInput: '#tosee',
+        notesInput: '#reason',
         submitBtn: 'input.btn:nth-child(1)',
         checkboxHeader: '.setChecked > a:nth-child(1) > img:nth-child(1)',
-        codeHeader: '.se-bg-gray > td:nth-child(2)',
-        studentNameHeader: '.se-bg-gray > td:nth-child(3)',
-        gradeHeader: '.se-bg-gray > td:nth-child(4)',
-        reasonHeader: '.se-bg-gray > td:nth-child(5)',
-        checkOutHeader: '.se-bg-gray > td:nth-child(6)',
-        checkInHeader: '.se-bg-gray > td:nth-child(7)'
+        nameHeader: '.se-bg-gray > td:nth-child(2)',
+        toSeeHeader: '.se-bg-gray > td:nth-child(3)',
+        reasonHeader: '.se-bg-gray > td:nth-child(4)',
+        checkInHeader: 'td.se-center:nth-child(5)',
+        checkOutHeader: 'td.se-center:nth-child(6)'
     }
 };
