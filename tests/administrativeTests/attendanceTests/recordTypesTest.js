2ade12c4570f405f90bc91f48e8016165912e171
Ray Wagner <raywagner88@me.com>
2018-07-02 12:41:44 -0500

    Add record type test

---
+++ tests/administrativeTests/attendanceTests/recordTypesTest.js
@@ -0,0 +1,48 @@
+module.exports = {
+    tags: ['administrativeTests', 'attendanceTests', 
+        'attendanceRecordTypesTest'],
+
+    'Attendance Record Types Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@administrative')
+            .click('@administrative')
+            .assert.visible('@attendance')
+            .click('@attendance')
+            .assert.visible('@attendanceRecordTypes')
+            .click('@attendanceRecordTypes')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.administrative.attendance.recordTypes()
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 
+                'Administrative/Attendance/ Record Types')
+
+            .activeTab('Current')
+
+            .assert.visible('@newTypeBtn')
+            .assert.attributeEquals('@newTypeBtn', 'value', 'New Type')
+            .assert.attributeEquals('@newTypeBtn', 'onclick', 'addtype()')
+
+            .assert.visible('@numericHeader')
+            .assert.containsText('@numericHeader', '#')
+
+            .assert.visible('@nameHeader')
+            .assert.containsText('@nameHeader', 'Name')
+
+            .assert.visible('@descriptionHeader')
+            .assert.containsText('@descriptionHeader', 'Description')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
