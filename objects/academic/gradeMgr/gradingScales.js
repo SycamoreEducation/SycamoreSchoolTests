21ed28d370d44ab7ea869358c77ab2aa2f1a1f75
Ray Wagner <raywagner88@me.com>
2018-07-11 12:30:54 -0500

    Add grading scales test

---
+++ objects/academic/gradeMgr/gradingScales.js
@@ -0,0 +1,10 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        newScaleBtn: '.btn',
+        nameHeader: '.se-bg-gray > td:nth-child(1)',
+        descriptionHeader: '.se-bg-gray > td:nth-child(2)',
+        globalHeader: '.se-bg-gray > td:nth-child(3)',
+        roundingHeader: '.se-bg-gray > td:nth-child(4)'
+    }
+};
