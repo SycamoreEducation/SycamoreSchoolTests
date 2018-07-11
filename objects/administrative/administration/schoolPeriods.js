6bb005edfbaa52ec88b2c1ac45127d177838247d
Ray Wagner <raywagner88@me.com>
2018-07-11 12:52:37 -0500

    Add school periods test

---
+++ objects/administrative/administration/schoolPeriods.js
@@ -0,0 +1,12 @@
+module.exports = {
+    elements: {
+        breadcrumb: 'body > ul.breadcrumb',
+        newPeriodBtn: '.btn',
+        numericHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1)',
+        codeHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)',
+        descriptionHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)',
+        startTimeHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(4)',
+        stopTimeHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(5)',
+        valueHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(6)'
+    }
+};
