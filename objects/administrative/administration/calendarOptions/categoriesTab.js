7cf365a0c5c433f41c670dff2970f67547dbd104
Ray Wagner <raywagner88@me.com>
2018-07-11 15:12:08 -0500

    Initial test build

---
+++ objects/administrative/administration/calendarOptions/categoriesTab.js
@@ -0,0 +1,8 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        importsTab: '.nav > li:nth-child(2) > a:nth-child(1)',
+        daysOfWeekTab: '.nav > li:nth-child(3) > a:nth-child(1) > font:nth-child(1)',
+        specialsDaysTab: '.nav > li:nth-child(4) > a:nth-child(1)'
+    }
+};
---
+++ tests/administrativeTests/administrationTests/calendarOptionsTests/categoriesTabTest.js
@@ -0,0 +1,46 @@
+module.exports = {
+    tags: ['administrativeTests', 'administrationTests', 'calendarOptionsTests',
+        'calendarOptionsCategoriesTabTest'],
+
+    'Calendar Options Categoreis Tab Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@administrative')
+            .click('@administrative')
+            .assert.visible('@administration')
+            .click('@administration')
+            .assert.visible('@administrationCalendarOptions')
+            .click('@administrationCalendarOptions')
+        client.frame(null);
+
+        client.frame(2);
+        client.page.administrative.administration.calendarOptions
+            .categoriesTab()
+
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 
+                'Administrative/Administration/ Calendar Options')
+
+            .activeTab('Categories')
+
+            .assert.visible('@importsTab')
+            .assert.containsText('@importsTab', 'Imports')
+
+            .assert.visible('daysOfWeekTab')
+            .assert.containsText('@daysOfWeekTab', 'Days of Week')
+
+            .assert.visible('@specialsDaysTab')
+            .assert.containsText('@specialsDaysTab', 'Specials Days')
+
+        client.frame(null)
+
+        client.end();
+    }
+};
