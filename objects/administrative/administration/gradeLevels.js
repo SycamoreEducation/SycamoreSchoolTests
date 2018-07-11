3e87e1c9d699009c643de2d59aa7466b31218f13
Ray Wagner <raywagner88@me.com>
2018-07-11 14:22:03 -0500

    Add grade levels test

---
+++ objects/administrative/administration/gradeLevels.js
@@ -0,0 +1,15 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        updateBtn: '.btn',
+        gradesTitle: 'table.table:nth-child(5) > tbody:nth-child(1) > tr:nth-child(1)',
+        lowestGradeLabel: 'table.table:nth-child(5) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1)',
+        lowestGradeSelect: '#firstgrade',
+        highestGradeLabel: 'table.table:nth-child(5) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1)',
+        highestGradeSelect: '#lastgrade',
+        namesTab: 'ul.nav:nth-child(6) > li:nth-child(1) > a:nth-child(1)',
+        gradeHeader: 'table.table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1)',
+        labelHeader: 'table.table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)',
+        externalHeader: 'table.table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)'
+    }
+};
