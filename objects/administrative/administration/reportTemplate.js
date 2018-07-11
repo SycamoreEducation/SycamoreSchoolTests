11beb96a5b3b06f60b040bf7d5932fbda89f23d8
Ray Wagner <raywagner88@me.com>
2018-07-11 13:11:19 -0500

    Add report template test

---
+++ objects/administrative/administration/reportTemplate.js
@@ -0,0 +1,10 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        newTemplateBtn: '.btn',
+        areaLabel: 'td.se-font',
+        areaSelect: '#tagid',
+        nameHeader: '.se-bg-gray > td:nth-child(2)',
+        descriptionHeader: '.se-bg-gray > td:nth-child(3)'
+    }
+};
