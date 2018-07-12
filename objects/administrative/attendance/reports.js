c15005f0267b6ea15617900722738db2a43ad4fd
Ray Wagner <raywagner88@me.com>
2018-07-12 09:22:02 -0500

    Change selector

--- objects/administrative/attendance/reports.js
+++ objects/administrative/attendance/reports.js
@@ -5,7 +5,7 @@ module.exports = {
         customTab: '.nav > li:nth-child(2) > a',
         nameHeader: '.se-bg-gray > td:nth-child(1)',
         descriptionHeader: '.se-bg-gray > td:nth-child(2)',
-        testReportName: '.se-font > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1)',
+        testReportName: '.se-font > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1) > a:nth-child(1)',
         testReportDescription: '.se-font > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2)'
     }
 };
