a922bdfefbc08809f194dbc85e6169d41136a9d3
Ray Wagner <raywagner88@me.com>
2018-07-13 09:07:23 -0500

    Add grade mgr attributes grades test

---
+++ objects/academic/gradeMgr/attributesGrades.js
@@ -0,0 +1,15 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        keysBtn: 'input.btn:nth-child(4)',
+        groupsBtn: 'input.btn-primary:nth-child(3)',
+        newAttributeBtn: 'input.pull-right:nth-child(2)',
+        gradeSelect: '#grade',
+        copyGradeSelect: '#copygrade',
+        copyBtn: 'body > form:nth-child(4) > table:nth-child(4) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > font:nth-child(1) > b:nth-child(1) > input:nth-child(2)',
+        deleteBtn: '.btn-danger',
+        checkboxHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > a:nth-child(1)',
+        numericHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)',
+        attributesHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)'
+    }
+};
