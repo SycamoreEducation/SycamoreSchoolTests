1e776e8ccfa66b7bf2bd98971b96061b43152261
Ray Wagner <raywagner88@me.com>
2018-07-16 13:50:02 -0500

    Update to global var

--- tests/academicTests/gradeMgrTests/additionalFieldsTest.js
+++ tests/academicTests/gradeMgrTests/additionalFieldsTest.js
@@ -20,7 +20,7 @@ module.exports = {
         client.frame(null);
 
         client.frame(2);
-        client.pause(100);
+        client.pause(client.globals.pauseTime);
         client.page.academic.gradeMgr.additionalFields()
             .waitForElementVisible('@breadcrumb', 5000)
             .assert.visible('@breadcrumb')
