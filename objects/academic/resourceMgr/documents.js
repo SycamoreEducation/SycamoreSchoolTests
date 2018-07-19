35f4cd417bff701167b368eea734045cc9eceb51
Ray Wagner <raywagner88@me.com>
2018-07-19 15:13:31 -0500

    Add resource mgr documents test

---
+++ objects/academic/resourceMgr/documents.js
@@ -0,0 +1,14 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        userLabel: 'body > form:nth-child(4) > table:nth-child(5) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > font',
+        userSelect: '.input-large',
+        reassignLabel: 'body > form:nth-child(4) > table:nth-child(5) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > font',
+        reassignSelect: '#duid',
+        moveBtn: '.btn',
+        checkboxHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > a',
+        dateTimeHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)',
+        filenameHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)',
+        descriptionHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(4)'
+    }
+};
