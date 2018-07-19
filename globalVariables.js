2678027c7bee9973e84b3897c11faee67415c493
Ray Wagner <raywagner88@me.com>
2018-07-19 12:52:57 -0500

    Split reporter files

--- globalVariables.js
+++ globalVariables.js
@@ -1,15 +1,5 @@
 require('dotenv').load();
-
-var HtmlReporter = require('nightwatch-html-reporter');
-var reporter = new HtmlReporter({
-    openBrowser: true,
-    reportsDirectory: __dirname + '/reports',
-    themeName: 'default-gray',
-    hideSuccess: true,
-    reportFilename: 'nightwatch.html',
-    uniqueFilename: true,
-    relativeScreenshots: true
-});
+var reporter = require('./htmlReporter').reporter;
 
 module.exports = {
     FRAME_PAUSE: process.env.FRAME_PAUSE || 500,
