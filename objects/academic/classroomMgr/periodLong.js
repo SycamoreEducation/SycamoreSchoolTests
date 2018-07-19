2f506d29edb9dbb59bcf3b5d7dacd337c30e41c3
Ray Wagner <raywagner88@me.com>
2018-07-19 10:33:21 -0500

    Add classroom mgr period long test

---
+++ objects/academic/classroomMgr/periodLong.js
@@ -0,0 +1,27 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        dayLongTab: '.nav > li:nth-child(1)',
+        generalTab: '.nav > li:nth-child(3)',
+        printBtn: 'button.btn:nth-child(5)',
+        newClassBtn: 'input.btn',
+        semesterSelect: 'select.small:nth-child(1)',
+        departmentSelect: 'select.small:nth-child(2)',
+        nameHeader: '.se-bg-gray > td:nth-child(2)',
+        sectionHeader: '.se-bg-gray > td:nth-child(3)',
+        homeHeader: '.se-bg-gray > td:nth-child(4)',
+        semester1Header: '.se-bg-gray > td:nth-child(5)',
+        semester2Header: '.se-bg-gray > td:nth-child(6)',
+        sunHeader: '.se-bg-gray > td:nth-child(7)',
+        monHeader: '.se-bg-gray > td:nth-child(8)',
+        tueHeader: '.se-bg-gray > td:nth-child(9)',
+        wedHeader: '.se-bg-gray > td:nth-child(10)',
+        thuHeader: '.se-bg-gray > td:nth-child(11)',
+        friHeader: '.se-bg-gray > td:nth-child(12)',
+        satHeader: '.se-bg-gray > td:nth-child(13)',
+        primaryTeacherHeader: '.se-bg-gray > td:nth-child(14)',
+        linkedHeader: '.se-bg-gray > td:nth-child(15)',
+        numericHeader: '.se-bg-gray > td:nth-child(16)',
+        activityHeader: '.se-bg-gray > td:nth-child(17)'
+    }
+};
