e1deca3dfcc4a8ce8cf6acc1b21b912526a492de
Ray Wagner <raywagner88@me.com>
2018-07-23 12:06:53 -0500

    Add curriculum mgr by subject test

---
+++ objects/academic/curriculumMgr/bySubject.js
@@ -0,0 +1,13 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        byGradeTab: '.nav > li:nth-child(2) > a',
+        newCourseBtn: '.btn',
+        subjectLabel: 'body > form:nth-child(4) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > font',
+        subjectSelect: '.small',
+        codeHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)',
+        courseNameHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)',
+        descriptionHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(4)',
+        assignmentCountHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(5)'
+    }
+};
