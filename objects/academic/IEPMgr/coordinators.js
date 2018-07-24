cd3c4f69ac6bc86f98029cf0992b96eed46d2cc1
Ray Wagner <raywagner88@me.com>
2018-07-24 08:38:07 -0500

    Add IEP Mgr Coordinators test

---
+++ objects/academic/IEPMgr/coordinators.js
@@ -0,0 +1,10 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        manageBtn: '.btn',
+        PANHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > img',
+        nameHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)',
+        titleHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)',
+        phoneHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(4)'
+    }
+};
