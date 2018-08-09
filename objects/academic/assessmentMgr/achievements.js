a34f6a6d3556452f23e9d1908cbd98dd3f426d11
Ray Wagner <raywagner88@me.com>
2018-08-09 11:19:39 -0500

    Add assessment mgr achievements test

---
+++ objects/academic/assessmentMgr/achievements.js
@@ -0,0 +1,13 @@
+module.exports = {
+    elements: {
+        breadcrumb: '.breadcrumb',
+        newAchievementBtn: '.btn',
+        schoolYearLabel: '.tab-content > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1)',
+        schoolYearSelect: '#schoolyearid',
+        titleHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)',
+        descriptionHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(5)',
+        schoolAvgHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(6)',
+        nationalAvgHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(7)',
+        countHeader: '.table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(8)'
+    }
+};
---
+++ tests/academicTests/assessmentMgrTests/achievementsTest.js
@@ -0,0 +1,65 @@
+module.exports = {
+    tags: ['academicTests', 'academicAssessmentMgrTests', 
+        'academicAssessmentMgrAchievementsTest'],
+
+    'Academic Assessment Mgr Achievements Test': (client) => {
+
+        var page = client.page.login();
+
+        page.navigate()
+            .schoolLogin();
+
+        client.frame(1);
+        client.page.menu()
+            .assert.visible('@academic')
+            .click('@academic')
+            .assert.visible('@assessmentMgr')
+            .click('@assessmentMgr')
+            .assert.visible('@assessmentMgrAchievements')
+            .click('@assessmentMgrAchievements');
+        client.frame(null);
+
+        client.frame(2);
+        client.pause(client.globals.FRAME_PAUSE);
+        client.page.academic.assessmentMgr.achievements()
+            .waitForElementVisible('@breadcrumb', 5000)
+            .assert.visible('@breadcrumb')
+            .assert.containsText('@breadcrumb', 
+                'Academic/Assessment Mgr/ Achievements')
+
+            .activeTab('2017-2018')
+
+            .assert.visible('@newAchievementBtn')
+            .assert.attributeEquals('@newAchievementBtn', 'value',
+                'New Achievement')
+            .assert.attributeEquals('@newAchievementBtn', 'onclick',
+                'newachievement()')
+
+            .assert.visible('@schoolYearLabel')
+            .assert.containsText('@schoolYearLabel', 'School Year')
+
+            .assert.visible('@schoolYearSelect')
+            .assert.containsText('@schoolYearSelect', '2017-2018')
+            .assert.attributeEquals('@schoolYearSelect', 'onchange',
+                'DoRefresh()')
+
+            .assert.visible('@titleHeader')
+            .assert.containsText('@titleHeader', 'Title')
+
+            .assert.visible('@descriptionHeader')
+            .assert.containsText('@descriptionHeader', 'Description')
+
+            .assert.visible('@schoolAvgHeader')
+            .assert.containsText('@schoolAvgHeader', 'School Avg')
+
+            .assert.visible('@nationalAvgHeader')
+            .assert.containsText('@nationalAvgHeader', 'National Avg')
+
+            .assert.visible('@countHeader')
+            .assert.containsText('@countHeader', 'Count');
+
+        client.frame(null);
+
+        client.end();
+    }
+};
