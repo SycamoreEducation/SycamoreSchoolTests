6385e7404f175444018ec2f82e4506b7dea6b1b3
Ray Wagner <raywagner88@me.com>
2018-08-10 10:12:04 -0500

    Add school year globals

--- globalVariables.js
+++ globalVariables.js
@@ -1,5 +1,7 @@
 require('dotenv').load();
 
 module.exports = {
-    FRAME_PAUSE: process.env.FRAME_PAUSE || 500
+    FRAME_PAUSE: process.env.FRAME_PAUSE || 500,
+    YEAR_START: process.env.YEAR_START,
+    YEAR_END: process.env.YEAR_END
 };
